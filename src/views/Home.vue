a31c57a69cf61f1f744a07a586d47d2876c68526
Tai Sakuma <tai.sakuma@gmail.com>
2020-11-15 11:51:22 -0500

    commit snapshot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,31 +8,35 @@
               <v-btn color="primary" @click="run()">Run</v-btn>
               <v-btn color="primary" @click="reset()">Reset</v-btn>
             </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card>
             <v-card-text
-              ><pre>{{ state }}</pre></v-card-text
+              ><pre>{{ state.state }}</pre></v-card-text
             >
           </v-card>
         </v-col>
-        <v-col>
-          <v-card>
-            <v-card-text>
-              <v-container fluid class="ma-0 pa-0">
-                <v-row>
-                  <v-col cols="1">
-                    <pre v-for="i in nlines" :key="i">{{ i }}</pre>
-                  </v-col>
-                  <v-col>
-                    <pre>{{ code }}</pre>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-          </v-card>
-        </v-col>
+      </v-row>
+      <v-row>
+        <template v-if="state.threads">
+          <template v-for="th in state.threads">
+            <template v-for="ta in th.tasks">
+              <v-col cols="4" v-if="ta.fileName" :key="th.threadId + '-' + ta.taskId">
+                <v-card>
+                  <v-card-text>
+                    <v-container fluid class="ma-0 pa-0">
+                      <v-row>
+                        <v-col cols="1">
+                          <pre v-for="i in ta.fileLines.length" :key="i">{{ i }} <span v-if="i==ta.lineNo">-></span> </pre>
+                        </v-col>
+                        <v-col cols=11>
+                          <pre>{{ ta.fileLines.join("\n") }}</pre>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </template>
+          </template>
+        </template>
       </v-row>
     </v-container>
   </div>
@@ -54,6 +58,14 @@ const subscState = gql`
       nthreads
       threads {
         threadId
+        tasks {
+          taskId
+          finished
+          prompting
+          fileName
+          lineNo
+          fileLines
+        }
       }
     }
   }
