f9fd55f97b773b7eccfe153005528164cf0d4eb0
Tai Sakuma <tai.sakuma@gmail.com>
2021-06-14 15:45:31 -0400

    add new component ScriptEditor

---
+++ src/components/ScriptEditor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div><v-textarea :value="source"></v-textarea></div>
+</template>
+
+<script>
+import QUERY_SOURCE from "@/graphql/queries/Source.gql";
+
+export default {
+  name: "ScriptEditor",
+  data: () => ({
+    sourceLines: [],
+  }),
+  apollo: {
+    sourceLines: {
+      query: QUERY_SOURCE,
+      update(data) {
+        return data.source;
+      },
+    },
+  },
+  computed: {
+    source() {
+      return this.sourceLines.join("\n");
+    },
+  },
+};
+</script>
