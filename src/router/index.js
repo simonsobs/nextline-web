c665f5d9d433f0d7eef66a5966c62222a8e235ba
Tai Sakuma <tai.sakuma@gmail.com>
2021-07-23 13:14:18 -0400

    add scratch

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "@/views/About.vue")
+  },
+  {
+    path: "/scratch",
+    name: "Scratch",
+    component: () =>
+      import(/* webpackChunkName: "scratch" */ "@/views/Scratch.vue")
   }
 ];
 
---
+++ src/views/Scratch.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid fill-height>
+    <v-row class="flex-nowrap" style="flex: 1 1 0">
+      <v-col style="flex: 1 1 0; min-width: 0">
+        <v-card flat outlined class="overflow-x-auto">
+          <pre>ABCDEFGHIJKLMNOPQRSTUVWXYZ ABCDEFGHIJKLMNOPQRSTUVWXYZ</pre>
+        </v-card>
+      </v-col>
+      <v-col style="flex: 1 1 0; min-width: 0">
+        <v-card flat outlined class="overflow-x-auto">
+          <pre>ABCDEFGHIJKLMNOPQRSTUVWXYZ ABCDEFGHIJKLMNOPQRSTUVWXYZ</pre>
+        </v-card>
+      </v-col>
+      <v-col style="flex: 1 1 0; min-width: 0">
+        <v-card flat outlined class="overflow-x-auto">
+          <pre>ABCDEFGHIJKLMNOPQRSTUVWXYZ ABCDEFGHIJKLMNOPQRSTUVWXYZ</pre>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
