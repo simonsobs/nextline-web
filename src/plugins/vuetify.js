1c746f4a6d0b525646871e08a3795c2aeffabae8
Tai Sakuma <tai.sakuma@gmail.com>
2021-12-08 13:13:58 -0500

    set themes in Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,20 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: false,
+    options: { customProperties: true },
+    themes: {
+      light: {
+        primary: colors.teal.darken2,
+        "on-primary": colors.grey.lighten5,
+        secondary: "#b0bec5",
+        anchor: "#8c9eff",
+      },
+    },
+  },
+});
