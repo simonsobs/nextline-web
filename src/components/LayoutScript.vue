7825ef9829b2e1c43050d1305e154400a2843920
Tai Sakuma <tai.sakuma@gmail.com>
2022-01-11 16:43:55 -0500

    extract component LayoutScript from Home

---
+++ src/components/LayoutScript.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="fill-height">
+    <v-container
+      v-if="nextlineState == 'running'"
+      fluid
+      fill-height
+      pa-0
+      class="align-stretch"
+    >
+      <v-row v-if="layout == 'grid'">
+        <v-col
+          :cols="cols"
+          :md="md"
+          :lg="lg"
+          v-for="threadTaskId in threadTaskIds"
+          :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
+        >
+          <script-exec-ctrl-int
+            :threadId="threadTaskId.threadId"
+            :taskId="threadTaskId.taskId"
+          ></script-exec-ctrl-int>
+        </v-col>
+      </v-row>
+      <v-row v-else class="fill-height flex-column flex-nowrap justify-start">
+        <v-col class="flex-grow-0 py-0 pr-5">
+          <v-tabs show-arrows v-model="tab">
+            <v-tab
+              v-for="threadTaskId in threadTaskIds"
+              :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
+            >
+              {{ threadTaskId.threadId }}
+              <span v-if="threadTaskId.taskId">
+                -{{ threadTaskId.taskId }}
+              </span>
+            </v-tab>
+          </v-tabs>
+        </v-col>
+        <v-col>
+          <v-tabs-items v-model="tab" class="fill-height">
+            <v-tab-item
+              v-for="threadTaskId in threadTaskIds"
+              :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
+              class="fill-height"
+            >
+              <script-exec-ctrl-int
+                :threadId="threadTaskId.threadId"
+                :taskId="threadTaskId.taskId"
+              ></script-exec-ctrl-int>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-col>
+      </v-row>
+    </v-container>
+    <script-viewer
+      v-else
+      :value="editing"
+      @input="$emit('editing-change', $event)"
+    ></script-viewer>
+  </div>
+</template>
+
+
+<script>
+import ScriptExecCtrlInt from "@/components/ScriptExecCtrlInt.vue";
+import ScriptViewer from "@/components/ScriptViewer.vue";
+
+export default {
+  name: "LayoutScript",
+  components: {
+    ScriptExecCtrlInt,
+    ScriptViewer,
+  },
+  props: {
+    editing: Boolean,
+    nextlineState: String,
+    threadTaskIds: Array,
+    layout: String,
+  },
+  data() {
+    return {
+      tab: null,
+    };
+  },
+  computed: {
+    cols() {
+      return 12;
+    },
+    md() {
+      if (this.threadTaskIds.length <= 1) return 12;
+      else return 6;
+    },
+    lg() {
+      if (this.threadTaskIds.length <= 1) return 12;
+      else if (this.threadTaskIds.length == 2) return 6;
+      else return 4;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,61 +17,13 @@
         <exception v-model="exception"></exception>
       </v-col>
       <v-col style="min-height: 240px">
-        <v-container
-          v-if="nextlineState == 'running'"
-          fluid
-          fill-height
-          pa-0
-          class="align-stretch"
-        >
-          <v-row v-if="layout == 'grid'">
-            <v-col
-              :cols="cols"
-              :md="md"
-              :lg="lg"
-              v-for="threadTaskId in threadTaskIds"
-              :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
-            >
-              <script-exec-ctrl-int
-                :threadId="threadTaskId.threadId"
-                :taskId="threadTaskId.taskId"
-              ></script-exec-ctrl-int>
-            </v-col>
-          </v-row>
-          <v-row
-            v-else
-            class="fill-height flex-column flex-nowrap justify-start"
-          >
-            <v-col class="flex-grow-0 py-0 pr-5">
-              <v-tabs show-arrows v-model="tab">
-                <v-tab
-                  v-for="threadTaskId in threadTaskIds"
-                  :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
-                >
-                  {{ threadTaskId.threadId }}
-                  <span v-if="threadTaskId.taskId">
-                    -{{ threadTaskId.taskId }}
-                  </span>
-                </v-tab>
-              </v-tabs>
-            </v-col>
-            <v-col>
-              <v-tabs-items v-model="tab" class="fill-height">
-                <v-tab-item
-                  v-for="threadTaskId in threadTaskIds"
-                  :key="`${threadTaskId.threadId}-${threadTaskId.taskId}`"
-                  class="fill-height"
-                >
-                  <script-exec-ctrl-int
-                    :threadId="threadTaskId.threadId"
-                    :taskId="threadTaskId.taskId"
-                  ></script-exec-ctrl-int>
-                </v-tab-item>
-              </v-tabs-items>
-            </v-col>
-          </v-row>
-        </v-container>
-        <script-viewer v-else v-model="editing"></script-viewer>
+        <layout-script
+          :editing="editing"
+          :nextline-state="nextlineState"
+          :thread-task-ids="threadTaskIds"
+          :layout="layout"
+          @editing-change="editing = $event"
+        ></layout-script>
       </v-col>
       <v-col style="max-height: 20vh">
         <stdout v-model="stdout"></stdout>
@@ -82,10 +34,9 @@
 
 <script>
 import MainCtrl from "@/components/MainCtrl.vue";
-import ScriptExecCtrlInt from "@/components/ScriptExecCtrlInt.vue";
-import ScriptViewer from "@/components/ScriptViewer.vue";
 import Stdout from "@/components/Stdout.vue";
 import Exception from "@/components/Exception.vue";
+import LayoutScript from "@/components/LayoutScript.vue";
 
 import SUBSCRIBE_GLOBAL_STATE from "@/graphql/subscriptions/GlobalState.gql";
 import SUBSCRIBE_THREAD_TASK_IDS from "@/graphql/subscriptions/ThreadTaskIds.gql";
@@ -94,15 +45,13 @@ export default {
   name: "Home",
   components: {
     MainCtrl,
-    ScriptExecCtrlInt,
-    ScriptViewer,
     Stdout,
     Exception,
+    LayoutScript
   },
   data: () => ({
     editing: false,
     layout: "grid", // "grid", "tabs"
-    tab: null,
     nextlineState: null,
     threadTaskIds: [],
     stdout: "",
@@ -122,20 +71,6 @@ export default {
           this.threadTaskIds = data.threadTaskIds;
         },
       },
-    },
-  },
-  computed: {
-    cols() {
-      return 12;
-    },
-    md() {
-      if (this.threadTaskIds.length <= 1) return 12;
-      else return 6;
-    },
-    lg() {
-      if (this.threadTaskIds.length <= 1) return 12;
-      else if (this.threadTaskIds.length == 2) return 6;
-      else return 4;
     },
   },
   methods: {
