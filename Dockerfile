d29ae6875f02c09ed13103a33855a2210986d4da
Tai Sakuma <tai.sakuma@gmail.com>
2022-02-03 21:04:24 -0500

    update Dockerfile and related files

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM node:16.13-alpine as build
 
+COPY ./ app
 WORKDIR /app
-COPY ./ src
-WORKDIR /app/src
 RUN npm install
 COPY docker/env.local .env.local
 RUN npm run build
@@ -11,11 +10,11 @@ RUN npm run build
 ##__________________________________________________________________||
 FROM nginx:1.21
 
+WORKDIR /app
 RUN apt-get update && apt-get install -y dumb-init
-COPY --from=build /app/src/dist /usr/share/nginx/html
-COPY docker/etc-nginx-conf.d-default.conf /etc/nginx/conf.d/default.conf
-COPY docker/cmd.sh /
-RUN chmod +x /cmd.sh
+COPY --from=build /app/dist site
+COPY docker/cmd.sh .
+COPY docker/nginx-default.conf /etc/nginx/conf.d/default.conf
 
 ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-CMD [ "/cmd.sh" ]
+CMD [ "./cmd.sh" ]
--- docker/cmd.sh
+++ docker/cmd.sh
@@ -4,10 +4,10 @@
 VUE_APP_GRAPHQL_HTTP_DEFAULT="http://localhost:8000"
 VUE_APP_PUBLIC_PATH_DEFAULT="/"
 
-VUE_APP_GRAPHQL_HTTP_PLACEHOLDER="graphql_api_url_placeholder_to_be_replaced"
-VUE_APP_PUBLIC_PATH_PLACEHOLDER="/vue_app_public_path_placeholder_to_be_replaced/"
+VUE_APP_GRAPHQL_HTTP_PLACEHOLDER="graphql_http_placeholder"
+VUE_APP_PUBLIC_PATH_PLACEHOLDER="/public_path_placeholder/"
 
-HTML_DIR="/usr/share/nginx/html"
+HTML_DIR="/app/site"
 
 ##__________________________________________________________________||
 if [ -z $VUE_APP_GRAPHQL_HTTP ]
@@ -33,13 +33,13 @@ fi
 
 if [ "$VUE_APP_PUBLIC_PATH" != "$VUE_APP_PUBLIC_PATH_DEFAULT" ]
 then
-    HTML_DIR_TEMP="/tmp/html"
+    HTML_DIR_TEMP="$(mktemp -d)/site"
     command="mv $HTML_DIR $HTML_DIR_TEMP"
     echo + $command
     eval $command
 
     HTML_DIR=$(echo $HTML_DIR/$VUE_APP_PUBLIC_PATH | tr -s /)
-    HTML_DIR=${HTML_DIR%/} # e.g., /usr/share/nginx/html/vue
+    HTML_DIR=${HTML_DIR%/} # e.g., /app/site/vue
     command="mkdir -p $(dirname $HTML_DIR)"
     echo + $command
     eval $command
@@ -55,19 +55,15 @@ fi
     $command
     for f in $(find . -type f);
     do
-        if [ -f $f ]; then
-            command="sed -i \"s#$VUE_APP_GRAPHQL_HTTP_PLACEHOLDER#$VUE_APP_GRAPHQL_HTTP#g\" $f";
-            echo + $command;
-            eval $command;
-        fi
+        command="sed -i \"s#$VUE_APP_GRAPHQL_HTTP_PLACEHOLDER#$VUE_APP_GRAPHQL_HTTP#g\" $f";
+        echo + $command;
+        eval $command;
     done
     for f in $(find . -type f);
     do
-        if [ -f $f ]; then
-            command="sed -i \"s#$VUE_APP_PUBLIC_PATH_PLACEHOLDER#$VUE_APP_PUBLIC_PATH#g\" $f";
-            echo + $command;
-            eval $command;
-        fi
+        command="sed -i \"s#$VUE_APP_PUBLIC_PATH_PLACEHOLDER#$VUE_APP_PUBLIC_PATH#g\" $f";
+        echo + $command;
+        eval $command;
     done
 )
 
