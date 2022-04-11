6cba15ea8c11f2c13ea64197fbff7980b7b38831
Tai Sakuma <tai.sakuma@gmail.com>
2022-04-11 14:39:59 -0400

    install manaco-editor

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const monacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 module.exports = {
+  configureWebpack: {
+    plugins: [new monacoWebpackPlugin()],
+  },
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.VUE_APP_PUBLIC_PATH
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
 };
