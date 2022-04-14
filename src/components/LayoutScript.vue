754c7152f1e4e35b6b99f487a54bd8132b379c45
Tai Sakuma <tai.sakuma@gmail.com>
2022-04-14 16:01:08 -0400

    replace Vuetify grid with CSS grid

--- src/components/LayoutScript.vue
+++ src/components/LayoutScript.vue
@@ -1,44 +1,33 @@
 <template>
   <div class="fill-height">
-    <v-container
+    <div
       v-if="nextlineState == 'running'"
-      fluid
-      fill-height
-      pa-0
-      class="align-stretch"
+      class="fill-height align-stretch pa-0"
     >
-      <v-row v-if="layout == 'grid'">
-        <v-col
-          :cols="cols"
-          :md="md"
-          :lg="lg"
+      <div v-if="layout == 'grid'" class="g-grid-container">
+        <code-exec
+          :traceId="traceId"
           v-for="traceId in traceIds"
           :key="traceId"
-        >
-          <code-exec :traceId="traceId"></code-exec>
-        </v-col>
-      </v-row>
-      <v-row v-else class="fill-height flex-column flex-nowrap justify-start">
-        <v-col class="flex-grow-0 py-0 pr-5">
-          <v-tabs show-arrows v-model="tab">
-            <v-tab v-for="traceId in traceIds" :key="traceId">
-              {{ traceId }}
-            </v-tab>
-          </v-tabs>
-        </v-col>
-        <v-col>
-          <v-tabs-items v-model="tab" class="fill-height">
-            <v-tab-item
-              v-for="traceId in traceIds"
-              :key="traceId"
-              class="fill-height"
-            >
-              <code-exec :traceId="traceId"></code-exec>
-            </v-tab-item>
-          </v-tabs-items>
-        </v-col>
-      </v-row>
-    </v-container>
+        ></code-exec>
+      </div>
+      <div v-else class="g-tab-container">
+        <v-tabs show-arrows v-model="tab">
+          <v-tab v-for="traceId in traceIds" :key="traceId">
+            {{ traceId }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab" class="fill-height">
+          <v-tab-item
+            v-for="traceId in traceIds"
+            :key="traceId"
+            class="fill-height"
+          >
+            <code-exec :traceId="traceId"></code-exec>
+          </v-tab-item>
+        </v-tabs-items>
+      </div>
+    </div>
     <script-editor v-else v-model="editing"></script-editor>
   </div>
 </template>
@@ -85,18 +74,6 @@ export default {
     layout() {
       return this.$store.state.layout;
     },
-    cols() {
-      return 12;
-    },
-    md() {
-      if (this.traceIds.length <= 1) return 12;
-      else return 6;
-    },
-    lg() {
-      if (this.traceIds.length <= 1) return 12;
-      else if (this.traceIds.length == 2) return 6;
-      else return 4;
-    },
   },
   watch: {
     editing(val) {
@@ -106,5 +83,17 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.g-grid-container {
+  display: grid;
+  height: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  gap: 6px;
+}
+.g-tab-container {
+  display: grid;
+  height: 100%;
+  grid-template-rows: min-content 1fr;
+  row-gap: 3px;
+}
 </style>
