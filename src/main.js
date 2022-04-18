bda30676d571c9dbfe395dd09e385c1493e1d163
Tai Sakuma <tai.sakuma@gmail.com>
2022-04-18 14:14:25 -0400

    replace Vuex with Pinia

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { createProvider } from "./vue-apollo";
+import { createPinia, PiniaVuePlugin } from 'pinia'
 
+Vue.use(PiniaVuePlugin)
 Vue.config.productionTip = false;
+
+const pinia = createPinia()
 
 new Vue({
   router,
-  store,
   vuetify,
+  pinia,
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
