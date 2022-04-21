2c3269446c2a47001ea56c87f9269929c87cdab6
Tai Sakuma <tai.sakuma@gmail.com>
2022-04-20 20:30:26 -0400

    stop using immediately-invoked function
    This causes an error in built app

--- src/components/LayoutScript.vue
+++ src/components/LayoutScript.vue
@@ -23,43 +23,41 @@ import ScriptEditor from "@/components/ScriptEditor.vue";
 import SUBSCRIBE_STATE from "@/graphql/subscriptions/State.gql";
 import SUBSCRIBE_TRACE_IDS from "@/graphql/subscriptions/TraceIds.gql";
 
-(function () {
-  // https://github.com/microsoft/monaco-editor/issues/1762
-  // https://github.com/microsoft/monaco-editor/blob/main/src/basic-languages/python/python.ts
+// https://github.com/microsoft/monaco-editor/issues/1762
+// https://github.com/microsoft/monaco-editor/blob/main/src/basic-languages/python/python.ts
 
-  const rules = [{ token: "keyword", foreground: "#0077aa" }];
-  const colors = {
-    "editor.foreground": "#000000CC",
-    "editor.background": "#FAFAFA",
-    "editorCursor.foreground": "#8B0000",
-    "editorLineNumber.foreground": "#9E9E9E",
-    "editor.selectionBackground": "#88000030",
-    "editor.inactiveSelectionBackground": "#88000015",
-  };
-  const colorsEditor = {
-    ...colors,
-    "editor.lineHighlightBackground": "#8F8F8F20",
-    "editorLineNumber.activeForeground": "#00796b",
-  };
-  const colorsViewer = {
-    ...colors,
-    "editorLineNumber.activeForeground": "#9E9E9E",
-  };
+const rules = [{ token: "keyword", foreground: "#0077aa" }];
+const colors = {
+  "editor.foreground": "#000000CC",
+  "editor.background": "#FAFAFA",
+  "editorCursor.foreground": "#8B0000",
+  "editorLineNumber.foreground": "#9E9E9E",
+  "editor.selectionBackground": "#88000030",
+  "editor.inactiveSelectionBackground": "#88000015",
+};
+const colorsEditor = {
+  ...colors,
+  "editor.lineHighlightBackground": "#8F8F8F20",
+  "editorLineNumber.activeForeground": "#00796b",
+};
+const colorsViewer = {
+  ...colors,
+  "editorLineNumber.activeForeground": "#9E9E9E",
+};
 
-  monaco.editor.defineTheme("nextline", {
-    base: "vs",
-    inherit: true,
-    rules,
-    colors: colorsEditor,
-  });
+monaco.editor.defineTheme("nextline", {
+  base: "vs",
+  inherit: true,
+  rules,
+  colors: colorsEditor,
+});
 
-  monaco.editor.defineTheme("nextline-viewer", {
-    base: "vs",
-    inherit: true,
-    rules,
-    colors: colorsViewer,
-  });
-})();
+monaco.editor.defineTheme("nextline-viewer", {
+  base: "vs",
+  inherit: true,
+  rules,
+  colors: colorsViewer,
+});
 
 export default {
   name: "LayoutScript",
