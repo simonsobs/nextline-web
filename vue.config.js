2ec5f77a5d1288e1be1412b9f4bc0b316692bfbd
Tai Sakuma <tai.sakuma@gmail.com>
2022-04-20 20:31:42 -0400

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+// https://github.com/microsoft/monaco-editor/blob/main/docs/integrate-esm.md
 const monacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
 module.exports = {
   configureWebpack: {
-    plugins: [new monacoWebpackPlugin()],
+    plugins: [new monacoWebpackPlugin({ languages: ["python"] })],
   },
   transpileDependencies: ["vuetify"],
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
