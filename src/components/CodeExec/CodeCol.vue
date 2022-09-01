12c727fee5adad0c90d31aea7bebdb7e77225a3e
Tai Sakuma <tai.sakuma@gmail.com>
2022-08-31 20:07:59 -0400

    rewrite CodeCol with composition api and urql

--- src/components/CodeExec/CodeCol.vue
+++ src/components/CodeExec/CodeCol.vue
@@ -1,117 +1,140 @@
 <template>
-  <div class="code-col" ref="editor"></div>
+  <div class="code-col" ref="refEditor"></div>
 </template>
 
-<script>
+<script lang="ts">
+import {
+  defineComponent,
+  PropType,
+  onMounted,
+  ref,
+  computed,
+  watch,
+  nextTick,
+} from "vue";
+import { useQuery } from "@urql/vue";
 import * as monaco from "monaco-editor";
 
 import QUERY_SOURCE from "@/graphql/queries/Source.gql";
 
-export default {
+interface PromptingData {
+  prompting: number;
+  fileName: string;
+  lineNo: number;
+  traceEvent: string;
+}
+
+export default defineComponent({
   name: "CodeCol",
   props: {
-    state: Object,
+    state: { type: Object as PropType<PromptingData>, required: true },
   },
-  data() {
+  setup(props) {
+    const refEditor = ref<HTMLElement | null>(null);
+
     const model = monaco.editor.createModel("", "python");
+
+    let editor: monaco.editor.IStandaloneCodeEditor | null = null;
+
+    onMounted(() => {
+      if (!refEditor.value) return;
+      editor = monaco.editor.create(refEditor.value, {
+        model,
+        minimap: { enabled: false },
+        scrollbar: { vertical: "auto", horizontal: "auto" },
+        fontFamily: "Fira Code",
+        fontSize: 14,
+        fontWeight: "500",
+        fontLigatures: true,
+        lineHeight: 24,
+        automaticLayout: true,
+        scrollBeyondLastLine: false,
+        glyphMargin: true,
+        readOnly: true,
+        matchBrackets: "never",
+        selectionHighlight: false,
+        occurrencesHighlight: false,
+        renderLineHighlight: "none",
+        theme: "nextline-viewer",
+      });
+    });
+
+    let decorationsCurrentLine: string[] = [];
+
+    function markCurrentLine() {
+      if (!editor) return;
+      const { lineNo, prompting } = props.state;
+      if (!(lineNo >= 1)) return;
+      decorationsCurrentLine = editor.deltaDecorations(decorationsCurrentLine, [
+        {
+          range: new monaco.Range(lineNo, 1, lineNo, 1),
+          options: {
+            isWholeLine: true,
+            className: prompting
+              ? "currentLineContent"
+              : "currentLineContentDim",
+            glyphMarginClassName: prompting
+              ? "currentLineMargin"
+              : "currentLineMarginDim",
+          },
+        },
+      ]);
+    }
+
+    function scroll() {
+      if (!editor) return;
+      const lineNo = props.state.lineNo;
+      if (!(lineNo >= 1)) return;
+      editor.revealLineInCenter(lineNo);
+    }
+
+    const fileName = ref(props.state.fileName);
+
+    watch(
+      () => props.state.fileName,
+      (val) => {
+        fileName.value = val;
+      }
+    );
+
+    const query = useQuery<{ source: string[] }>({
+      query: QUERY_SOURCE,
+      variables: {
+        fileName,
+      },
+    });
+
+    const sourceLines = ref<string[]>([]);
+
+    watch(query.data, (data) => {
+      if (!data?.source) return;
+      sourceLines.value = data.source;
+      nextTick(scroll);
+      nextTick(markCurrentLine);
+    });
+
+    watch(
+      () => props.state,
+      () => {
+        nextTick(scroll);
+        nextTick(markCurrentLine);
+      },
+      { immediate: true }
+    );
+
+    const source = computed(() => {
+      return sourceLines.value.join("\n");
+    });
+
+    watch(source, (val) => {
+      model.setValue(val);
+    });
+
     return {
-      sourceLines: [],
-      editor: null,
-      model,
-      decorationsCurrentLine: [],
+      refEditor,
     };
   },
-  computed: {
-    source() {
-      return this.sourceLines.join("\n");
-    },
-  },
-  apollo: {
-    sourceLines: {
-      query: QUERY_SOURCE,
-      variables() {
-        return {
-          fileName: this.state.fileName,
-        };
-      },
-      skip() {
-        return !this.state;
-      },
-      update(data) {
-        return data.source;
-      },
-      result() {
-        this.$nextTick(this.scroll);
-        this.$nextTick(this.markCurrentLine);
-      },
-    },
-  },
-  watch: {
-    source() {
-      this.model.setValue(this.source);
-    },
-    state: {
-      handler() {
-        this.$nextTick(this.scroll);
-        this.$nextTick(this.markCurrentLine);
-      },
-      immediate: true,
-    },
-  },
-  async mounted() {
-    const el = this.$refs.editor;
-    this.editor = monaco.editor.create(el, {
-      model: this.model,
-      minimap: { enabled: false },
-      scrollbar: { vertical: "auto", horizontal: "auto" },
-      fontFamily: "Fira Code",
-      fontSize: "14px",
-      fontWeight: 500,
-      fontLigatures: true,
-      lineHeight: "24px",
-      automaticLayout: true,
-      scrollBeyondLastLine: false,
-      glyphMargin: true,
-      readOnly: true,
-      matchBrackets: "never",
-      selectionHighlight: false,
-      occurrencesHighlight: false,
-      renderLineHighlight: "none",
-      theme: "nextline-viewer",
-    });
-    // this.$nextTick(this.markCurrentLine);
-  },
-  methods: {
-    markCurrentLine() {
-      if (!this.editor) return;
-      const { lineNo, prompting } = this.state;
-      if (!(lineNo >= 1)) return;
-      this.decorationsCurrentLine = this.editor.deltaDecorations(
-        this.decorationsCurrentLine,
-        [
-          {
-            range: new monaco.Range(lineNo, 1, lineNo, 1),
-            options: {
-              isWholeLine: true,
-              className: prompting
-                ? "currentLineContent"
-                : "currentLineContentDim",
-              glyphMarginClassName: prompting
-                ? "currentLineMargin"
-                : "currentLineMarginDim",
-            },
-          },
-        ]
-      );
-    },
-    scroll() {
-      if (!this.editor) return;
-      const lineNo = this.state.lineNo;
-      if (!(lineNo >= 1)) return;
-      this.editor.revealLineInCenter(lineNo);
-    },
-  },
-};
+});
 </script>
 
 <style>
