30c1a491f15abddaa4839423dfccf0df064cb24a
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-06 16:57:57 -0400

    set vue/script-setup-uses-vars in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "no-unused-vars": "off",
     "vue/no-unused-components": "off",
     "vue/multi-word-component-names": "off",
+    "vue/script-setup-uses-vars": 1,
     "@typescript-eslint/no-var-requires": "off",
   },
 };
