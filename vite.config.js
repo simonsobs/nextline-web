7872b102b6bbb7e576f9c938438cfdb4294e0d41
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-06 15:32:01 -0400

    install vite-plugin-simple-gql

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import path from "path";
+import gql from "vite-plugin-simple-gql";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), gql()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
