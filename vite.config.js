45474975b0e6d03256e995d2741e914f19c8ba2f
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-06 15:49:40 -0400

    use VuetifyResolver from unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import path from "path";
 import gql from "vite-plugin-simple-gql";
+import { VuetifyResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
 
 export default defineConfig({
-  plugins: [vue(), gql()],
+  plugins: [
+    vue(),
+    gql(),
+    Components({
+      resolvers: [VuetifyResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
