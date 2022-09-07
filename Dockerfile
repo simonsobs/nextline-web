0cef9078c7d81b48cdde99824521c2ca375bb3ed
Tai Sakuma <tai.sakuma@gmail.com>
2022-09-06 20:26:40 -0400

    correct public path so to work with a subfolder in docker

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN apt-get update && apt-get install -y jq
 
 WORKDIR /app
 COPY --from=build /app/dist dist
+COPY --from=build /app/dist/public_path_placeholder/monacoeditorwork dist/monacoeditorwork
 COPY docker/entrypoint.sh .
 COPY docker/setup.sh /docker-entrypoint.d/99-nextline-web-setup.sh
 COPY docker/nginx-default.conf.template /etc/nginx/templates/default.conf.template
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import loadVersion from "vite-plugin-package-version";
 import gql from "vite-plugin-simple-gql";
@@ -7,23 +7,29 @@ import Components from "unplugin-vue-components/vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
 import path from "path-browserify";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    loadVersion(),
-    gql(),
-    monacoEditorPlugin({
-      languageWorkers: ["editorWorkerService"],
-    }),
-    Components({
-      resolvers: [VuetifyResolver()],
-    }),
-  ],
-  base: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      path: "path-browserify",
+export default ({ mode }) => {
+  // loadEnv: https://stackoverflow.com/a/66389044/7309855
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      loadVersion(),
+      gql(),
+      monacoEditorPlugin({
+        languageWorkers: ["editorWorkerService"],
+        publicPath: "monacoeditorwork", // default
+      }),
+      Components({
+        resolvers: [VuetifyResolver()],
+      }),
+    ],
+    base: process.env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        path: "path-browserify",
+      },
     },
-  },
-});
+  });
+};
