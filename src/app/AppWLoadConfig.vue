29adaad3d7128de3d45373dfeb6ebf6aba6123bf
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-05 19:23:38 -0400

    add AppWLoadConfig

---
+++ src/app/AppWLoadConfig.vue
@@ -0,0 +1,11 @@
+<template>
+  <app v-if="!(loading || error)"> </app>
+  <div v-else-if="error">{{ error }}</div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useConfigStore } from "@/stores/config";
+import App from "./App.vue";
+const { loading, error } = storeToRefs(useConfigStore());
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import Vue from "vue";
-import App from "@/app/App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createPinia, PiniaVuePlugin, mapStores } from "pinia";
 import { defineThemes } from "./monaco-editor";
 import { useStore } from "@/stores/main";
+
+import App from "@/app/AppWLoadConfig.vue";
 
 Vue.use(PiniaVuePlugin);
 Vue.config.productionTip = false;
