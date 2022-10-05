3af2aa1ad0b04f4ffaa3b0a8d1901a8423dd9bfc
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-05 19:44:42 -0400

    remove config from the main store
    - moved to the config store

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createPinia, PiniaVuePlugin, mapStores } from "pinia";
 import { defineThemes } from "./monaco-editor";
-import { useStore } from "@/stores/main";
 
 import App from "@/app/AppWLoadConfig.vue";
 
@@ -16,12 +15,8 @@ new Vue({
   router,
   vuetify,
   pinia,
-  computed: {
-    ...mapStores(useStore),
-  },
   created() {
     defineThemes(this.$vuetify);
-    this.$nextTick(this.mainStore.loadConfig);
   },
   render: (h) => h(App),
 }).$mount("#app");
--- src/stores/main.ts
+++ src/stores/main.ts
@@ -1,25 +1,12 @@
 import { defineStore } from "pinia";
-import axios from "axios";
-import * as path from 'path';
 
 export const useStore = defineStore("main", {
   state: () => {
     return {
-      config: { apiName: null },
       modified: false,
     };
   },
   actions: {
-    async loadConfig() {
-      const url = path.join(import.meta.env.VITE_PUBLIC_PATH, "config.json");
-      try {
-        const response = await axios.get(url);
-        const config = response.data;
-        this.config = config;
-      } catch (error) {
-        // TODO: handle error
-      }
-    },
     setModified(v = true) {
       this.modified = v;
     },
