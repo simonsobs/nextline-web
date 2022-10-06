87bca864b47c9c05e49d9d18bfdc8d58d96d71b3
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-05 22:12:55 -0400

    rewrite MainCtrl with script setup and generated code

--- src/components/MainCtrl.vue
+++ src/components/MainCtrl.vue
@@ -50,105 +50,87 @@
   </v-card>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed, watch } from "vue";
-import { useMutation, useSubscription } from "@urql/vue";
+<script setup lang="ts">
+import { ref, computed, watch } from "vue";
 
 import { useStore } from "@/stores/main";
 
-import RESET from "@/graphql/mutations/Reset.gql";
-import EXEC from "@/graphql/mutations/Exec.gql";
-import INTERRUPT from "@/graphql/mutations/Interrupt.gql";
-import TERMINATE from "@/graphql/mutations/Terminate.gql";
-import KILL from "@/graphql/mutations/Kill.gql";
-import SUBSCRIBE_STATE from "@/graphql/subscriptions/State.gql";
-import SUBSCRIBE_TRACE_IDS from "@/graphql/subscriptions/TraceIds.gql";
+import {
+  useResetMutation,
+  useExecMutation,
+  useInterruptMutation,
+  useTerminateMutation,
+  useKillMutation,
+  useStateSubscription,
+  useTraceIdsSubscription,
+} from "@/gql/graphql";
 
-export default defineComponent({
-  name: "MainCtrl",
-  setup() {
-    const nextlineState = ref<string>("");
-    const stateSubscription = useSubscription<{ state: string }>({
-      query: SUBSCRIBE_STATE,
-    });
-    watch(stateSubscription.data, (val) => {
-      nextlineState.value = val?.state || "";
-    });
-
-    const traceIds = ref<number[]>([]);
-    const traceIdsSubscription = useSubscription<{ traceIds: number[] }>({
-      query: SUBSCRIBE_TRACE_IDS,
-    });
-    watch(traceIdsSubscription.data, (val) => {
-      traceIds.value = val?.traceIds || [];
-    });
-
-    const buttons = ref([
-      {
-        text: "Run",
-        method: "run",
-        icon: "mdi-play",
-        states: ["initialized"],
-      },
-      {
-        text: "Reset",
-        method: "reset",
-        icon: "mdi-restore",
-        states: ["initialized", "finished", "closed"],
-      },
-      {
-        text: "Interrupt",
-        method: "interrupt",
-        icon: "mdi-close",
-        states: ["running"],
-      },
-    ]);
-
-    const chipConfig = ref({
-      default: { color: null },
-      initialized: { color: "success" },
-      running: { color: "primary" },
-      exited: { color: "warning" },
-      finished: { color: "warning" },
-      closed: { color: "warning" },
-    });
-
-    const store = useStore();
-    const editing = computed(() => store.modified);
-
-    const chip = computed(
-      () => chipConfig.value[nextlineState.value] || chipConfig.value.default
-    );
-
-    async function onClick(method: string) {
-      if (method === "run") {
-        await executeExec();
-      } else if (method === "reset") {
-        await executeReset();
-      } else if (method === "interrupt") {
-        await executeInterrupt();
-      } else if (method === "terminate") {
-        await executeTerminate();
-      } else if (method === "kill") {
-        await executeKill();
-      }
-    }
-
-    const { executeMutation: executeExec } = useMutation(EXEC);
-    const { executeMutation: executeReset } = useMutation(RESET);
-    const { executeMutation: executeInterrupt } = useMutation(INTERRUPT);
-    const { executeMutation: executeTerminate } = useMutation(TERMINATE);
-    const { executeMutation: executeKill } = useMutation(KILL);
-
-    return {
-      nextlineState,
-      traceIds,
-      buttons,
-      chipConfig,
-      editing,
-      chip,
-      onClick,
-    };
-  },
+const nextlineState = ref<string>("");
+const stateSubscription = useStateSubscription();
+watch(stateSubscription.data, (val) => {
+  nextlineState.value = val?.state || "";
 });
+
+const traceIds = ref<number[]>([]);
+const traceIdsSubscription = useTraceIdsSubscription();
+watch(traceIdsSubscription.data, (val) => {
+  traceIds.value = val?.traceIds || [];
+});
+
+const buttons = ref([
+  {
+    text: "Run",
+    method: "run",
+    icon: "mdi-play",
+    states: ["initialized"],
+  },
+  {
+    text: "Reset",
+    method: "reset",
+    icon: "mdi-restore",
+    states: ["initialized", "finished", "closed"],
+  },
+  {
+    text: "Interrupt",
+    method: "interrupt",
+    icon: "mdi-close",
+    states: ["running"],
+  },
+]);
+
+const chipConfig = ref({
+  default: { color: null },
+  initialized: { color: "success" },
+  running: { color: "primary" },
+  exited: { color: "warning" },
+  finished: { color: "warning" },
+  closed: { color: "warning" },
+});
+
+const store = useStore();
+const editing = computed(() => store.modified);
+
+const chip = computed(
+  () => chipConfig.value[nextlineState.value] || chipConfig.value.default
+);
+
+async function onClick(method: string) {
+  if (method === "run") {
+    await executeExec({});
+  } else if (method === "reset") {
+    await executeReset({});
+  } else if (method === "interrupt") {
+    await executeInterrupt({});
+  } else if (method === "terminate") {
+    await executeTerminate({});
+  } else if (method === "kill") {
+    await executeKill({});
+  }
+}
+
+const { executeMutation: executeExec } = useExecMutation();
+const { executeMutation: executeReset } = useResetMutation();
+const { executeMutation: executeInterrupt } = useInterruptMutation();
+const { executeMutation: executeTerminate } = useTerminateMutation();
+const { executeMutation: executeKill } = useKillMutation();
 </script>
