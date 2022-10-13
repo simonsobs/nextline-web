78f328ba9fb96c3ed7ab40ee56c2e43d3d23a7cb
Tai Sakuma <tai.sakuma@gmail.com>
2022-10-13 13:51:46 -0400

    rename

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,8 @@ import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
-import Runs from "@/views/RunsView.vue";
-import Run from "@/views/RunView.vue";
+import RunsView from "@/views/RunsView.vue";
+import RunView from "@/views/RunView.vue";
 
 // Pinia must be plugged in before the router if a store is used in a route
 // https://github.com/vuejs/pinia/discussions/723#discussioncomment-2110660
@@ -26,12 +26,12 @@ const routes: Array<RouteConfig> = [
   {
     path: "/history/runs",
     name: "runs",
-    component: Runs,
+    component: RunsView,
   },
   {
     path: "/history/runs/:runNo",
     name: "run",
-    component: Run,
+    component: RunView,
   },
   {
     path: "/scratch",
