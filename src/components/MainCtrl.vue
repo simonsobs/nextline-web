a75c384ab69e263e65a1cb8679478132c42a8774
Tai Sakuma <tai.sakuma@gmail.com>
2023-04-11 10:20:18 -0400

    add ScheduleCtrl

--- src/components/MainCtrl.vue
+++ src/components/MainCtrl.vue
@@ -1,9 +1,11 @@
 <template>
   <v-card flat class="grey lighten-5 d-flex">
+    <schedule-ctrl></schedule-ctrl>
     <run-ctrl></run-ctrl>
   </v-card>
 </template>
 
 <script setup lang="ts">
+import ScheduleCtrl from "@/components/ScheduleCtrl.vue";
 import RunCtrl from "@/components/RunCtrl.vue";
 </script>
