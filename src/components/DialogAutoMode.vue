6a3d7d00e614f9c002300105aad6ae6ec43f53ca
Tai Sakuma <tai.sakuma@gmail.com>
2023-07-19 12:13:09 -0400

    move the auto mode button to a dialog

---
+++ src/components/DialogAutoMode.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card v-if="scheduleStore.autoMode == null" class="pa-1" max-width="290px">
+    <v-alert
+      type="error"
+      variant="tonal"
+      title="Error"
+      text="Unable to determine auto mode state."
+    >
+    </v-alert>
+  </v-card>
+  <v-card v-else-if="scheduleStore.autoMode" class="pa-1" max-width="290px">
+    <v-card-text>
+      <div class="text=h6 mb-1">
+        <p>
+          In auto mode, after each run, the next script is automatically loaded
+          from the scheduler and run.
+        </p>
+        <p style="margin-top: 1.5em">Turn off auto mode?</p>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined" @click="turnOff"> turn off </v-btn>
+    </v-card-actions>
+  </v-card>
+  <v-card v-else class="pa-1" max-width="290px">
+    <v-card-text>
+      <div class="text=h6 mb-1">
+        <p>
+          In auto mode, after each run, the next script is automatically loaded
+          from the scheduler and run.
+        </p>
+        <p style="margin-top: 1.5em">Turn on auto mode?</p>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined" @click="turnOn"> turn on </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { useScheduleStore } from "@/stores/schedule";
+import {
+  useAutoModeTurnOffMutation,
+  useAutoModeTurnOnMutation,
+} from "@/gql/graphql";
+
+const scheduleStore = useScheduleStore();
+const { executeMutation: turnOn } = useAutoModeTurnOnMutation();
+const { executeMutation: turnOff } = useAutoModeTurnOffMutation();
+</script>
--- src/components/ScheduleCtrl.vue
+++ src/components/ScheduleCtrl.vue
@@ -1,27 +1,25 @@
 <template>
   <v-card-actions>
-    <span class="text-capitalize mx-3">
-      {{ scheduleAutoModeState }}
-    </span>
-    <v-btn variant="outlined" @click="turnOn" v-if="scheduleAutoModeState == 'off'">
-      turn on
-    </v-btn>
-    <v-btn variant="outlined" @click="turnOff" v-else> turn off </v-btn>
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props"> Auto Mode: {{ label }} </v-btn>
+      </template>
+      <dialog-auto-mode> </dialog-auto-mode>
+    </v-menu>
   </v-card-actions>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
-import {
-  useAutoModeTurnOffMutation,
-  useAutoModeTurnOnMutation,
-  useScheduleAutoModeStateSubscription,
-} from "@/gql/graphql";
-const scheduleAutoModeStateSubscription =
-  useScheduleAutoModeStateSubscription();
-const scheduleAutoModeState = computed(
-  () => scheduleAutoModeStateSubscription.data?.value?.scheduleAutoModeState
-);
-const { executeMutation: turnOn } = useAutoModeTurnOnMutation();
-const { executeMutation: turnOff } = useAutoModeTurnOffMutation();
+import { useScheduleStore } from "@/stores/schedule";
+import DialogAutoMode from "@/components/DialogAutoMode.vue";
+
+const scheduleStore = useScheduleStore();
+
+const label = computed(() => {
+  const s = scheduleStore.autoMode;
+  if (s === false) return "Off";
+  if (s === true) return "On";
+  return "Unknown";
+});
 </script>
