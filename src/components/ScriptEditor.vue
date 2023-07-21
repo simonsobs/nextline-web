59d0238a8073786558facf6c801e4573fb226c52
Tai Sakuma <tai.sakuma@gmail.com>
2023-07-21 13:44:28 -0400

    change a color

--- src/components/ScriptEditor.vue
+++ src/components/ScriptEditor.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout full-height style="width: 100%">
-    <v-card flat class="g-container" rounded="0">
+    <v-card flat class="g-container bg-grey-lighten-4" rounded="0">
       <v-card-actions class="g-header flex-row flex-wrap">
         <v-btn
           variant="outlined"
