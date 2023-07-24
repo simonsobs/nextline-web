f77c50feba31ccee765bb1c7d35848b11cbf9640
Tai Sakuma <tai.sakuma@gmail.com>
2023-07-24 12:45:44 -0400

    add generated themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,22 +8,26 @@ import { aliases, mdi } from "vuetify/iconsets/mdi";
 // https://github.com/vuetifyjs/vuetify/issues/16346
 import colors from "vuetify/lib/util/colors.mjs";
 
-// Vue.use(Vuetify);
+import { fromSourceColor } from "./material-color-old";
+import { generateLightAndDarkThemesFromSourceColor } from "./material-color";
 
-// export default new Vuetify({
-//   theme: {
-//     dark: false,
-//     options: { customProperties: true, variations: true },
-//     themes: {
-//       light: {
-//         primary: colors.blueGrey.darken2,
-//         accent: colors.orange.base,
-//         background: colors.grey.base, // https://stackoverflow.com/a/59720255/7309855
-//       },
-//     },
-//   },
-// });
-//
+const baseColor: string = colors.blueGrey.darken2;
+
+const [dynamicLight, dynamicDark] =
+  generateLightAndDarkThemesFromSourceColor(baseColor);
+
+if (dynamicLight.colors)
+  dynamicLight.colors.accent = dynamicLight.colors.tertiary;
+
+if (dynamicDark.colors) dynamicDark.colors.accent = dynamicDark.colors.tertiary;
+
+const [materialLight, materialDark] = fromSourceColor(baseColor);
+
+if (materialLight.colors)
+  materialLight.colors.accent = materialLight.colors.tertiary;
+
+if (materialDark.colors)
+  materialDark.colors.accent = materialDark.colors.tertiary;
 
 const customLightTheme: ThemeDefinition = {
   dark: false,
@@ -41,7 +45,7 @@ const vuetify = createVuetify({
   directives,
   blueprint: md3,
   theme: {
-    defaultTheme: "customLightTheme",
+    defaultTheme: "dynamicLight",
     variations: {
       colors: ["primary", "accent", "surface", "background"],
       lighten: 5,
@@ -49,6 +53,10 @@ const vuetify = createVuetify({
     },
     themes: {
       customLightTheme,
+      materialLight,
+      materialDark,
+      dynamicLight,
+      dynamicDark,
     },
   },
   icons: {
