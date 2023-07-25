f047ce7461d73990e8f43acf987e257af0d8caad
Tai Sakuma <tai.sakuma@gmail.com>
2023-07-25 18:26:47 -0400

    update layout of RunConfirmationDialog

--- src/components/main/run-frame/top-bar/RunConfirmationDialog.vue
+++ src/components/main/run-frame/top-bar/RunConfirmationDialog.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-card>
+  <v-card class="pa-2">
     <v-card-title> Start running the script? </v-card-title>
-	<v-card-text> {{ apiName }}</v-card-text>
+    <v-card-text> {{ apiName }}</v-card-text>
     <v-card-text> Are you sure to start? </v-card-text>
     <v-card-actions>
       <v-btn variant="text" color="grey-darken-2" @click="$emit('cancel')">
-        Cancel
+        cancel
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn variant="text" color="primary" @click="$emit('confirm')">
-        Start
+      <v-btn variant="flat" color="primary" @click="$emit('confirm')">
+        start
       </v-btn>
     </v-card-actions>
   </v-card>
