60b025bbfba503043b404b9825d43fa6032a606d
Tai Sakuma <tai.sakuma@gmail.com>
2023-07-27 15:55:59 -0400

    display "Interactive"

--- src/components/main/run-frame/top-bar/SpanRunNoState.vue
+++ src/components/main/run-frame/top-bar/SpanRunNoState.vue
@@ -5,6 +5,12 @@
     <span class="text-capitalize text-primary font-weight-bold">
       {{ state || "unknown" }}
     </span>
+    <template v-if="state === 'running' && !continuousEnabled">
+    ⋅
+    <span class="text-capitalize text-primary font-weight-bold">
+      Interactive
+    </span>
+    </template>
   </span>
 </template>
 
@@ -14,10 +20,18 @@ import { computed } from "vue";
 import {
   useQStateQuery,
   useQRunNoQuery,
+  useQContinuousEnabledQuery,
   useStateSubscription,
   useRunNoSubscription,
+  useContinuousEnabledSubscription,
 } from "@/gql/graphql";
 
+const runNoQuery = useQRunNoQuery();
+const runNoSubscription = useRunNoSubscription();
+
+const runNo = computed(
+  () => runNoSubscription.data?.value?.runNo || runNoQuery.data?.value?.runNo
+);
 const stateQuery = useQStateQuery();
 const stateSubscription = useStateSubscription();
 
@@ -25,10 +39,12 @@ const state = computed(
   () => stateSubscription.data?.value?.state || stateQuery.data?.value?.state
 );
 
-const runNoQuery = useQRunNoQuery();
-const runNoSubscription = useRunNoSubscription();
+const continuousEnabledQuery = useQContinuousEnabledQuery();
+const continuousEnabledSubscription = useContinuousEnabledSubscription();
 
-const runNo = computed(
-  () => runNoSubscription.data?.value?.runNo || runNoQuery.data?.value?.runNo
+const continuousEnabled = computed(
+  () =>
+    continuousEnabledSubscription.data?.value?.continuousEnabled ||
+    continuousEnabledQuery.data?.value?.continuous.continuousEnabled
 );
 </script>
