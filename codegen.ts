59ce60f471930bd1f759e0ee20bc0517d7299679
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-21 15:41:32 -0400

    move gql files to gql folder

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://127.0.0.1:8000",
-  documents: ["src/graphql/**/*.gql"],
+  documents: ["src/graphql/gql/**/*.gql"],
   generates: {
     "src/gql/graphql.ts": {
       plugins: ["typescript", "typescript-operations", "typescript-vue-urql"],
