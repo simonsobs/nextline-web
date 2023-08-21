531fd03f175e3d1a5ac1af5a98ab8df17cfe4291
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-21 11:37:27 -0400

    rename App.vue to AppMain.vue

--- src/app/AppWProvideUrqlClient.vue
+++ src/app/AppWProvideUrqlClient.vue
@@ -7,7 +7,7 @@ import { computed } from "vue";
 import { provideClient } from "@urql/vue";
 import { createUrqlClient } from "@/urql";
 import { useConfigStore } from "@/stores/config";
-import App from "./App.vue";
+import App from "./AppMain.vue";
 const configStore = useConfigStore();
 const graphqlUrl = computed(() => configStore.config?.apiHttp);
 if (!graphqlUrl.value) throw new Error("No graphqlUrl");
