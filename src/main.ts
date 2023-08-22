c3b4c24e8928e91573e46cff8301a5551b845de4
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-22 10:14:49 -0400

    clean code

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,8 @@ import vuetify from "@/plugins/vuetify";
 
 import App from "@/app/App.vue";
 
-const app = createApp({
-  render: () => h(App),
-});
-app.use(pinia);
-app.use(router);
-app.use(vuetify);
-app.mount("#app");
+createApp({ render: () => h(App) })
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
