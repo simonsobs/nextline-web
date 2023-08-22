26b99ba075a06007abaa190525b835f4fae222b7
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-22 11:14:12 -0400

    add a doc

--- src/utils/config/provide-config.ts
+++ src/utils/config/provide-config.ts
@@ -4,6 +4,10 @@ import type { Config } from "./config";
 
 export const injectionKey: InjectionKey<Ref<Config>> = Symbol("config");
 
+/**
+ * Provide the config to the child components.
+ * In the child components, use `useConfig` to get the config.
+ */
 export function useProvideConfig(config: MaybeRefOrGetter<Config>) {
   const configRef = ref(toValue(config));
   watchEffect(() => {
