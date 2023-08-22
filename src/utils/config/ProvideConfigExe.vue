1f9c56a0cfdc4c75bdcf695bfd2119793c02fb5a
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-22 11:05:37 -0400

    make the config object reactive

--- src/utils/config/ProvideConfigExe.vue
+++ src/utils/config/ProvideConfigExe.vue
@@ -3,10 +3,16 @@
 </template>
 
 <script setup lang="ts">
+/**
+ * Provide the config to the slot.
+ * The config will be reactive.
+ */
+import { toRefs } from "vue";
 import { useProvideConfig, Config } from "@/utils/config";
 interface Props {
   config: Config;
 }
 const prop = defineProps<Props>();
-useProvideConfig(prop.config);
+const { config } = toRefs(prop);
+useProvideConfig(config);
 </script>
