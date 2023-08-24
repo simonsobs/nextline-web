d66ec358cb89c0555060bfa60dceed0b8b38c893
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-24 18:26:45 -0400

    clean code

--- src/utils/color-theme/material-color.ts
+++ src/utils/color-theme/material-color.ts
@@ -1,3 +1,24 @@
+/**
+ * The code to generate Dynamic Color of Material Design:
+ * https://m3.material.io/styles/color/dynamic-color/
+ *
+ * It uses material-color-utilities:
+ * https://github.com/material-foundation/material-color-utilities
+ *
+ * The code was originally based on dynamic_color_test.ts:
+ * https://github.com/material-foundation/material-color-utilities/blob/ce99247b6ba/typescript/dynamiccolor/dynamic_color_test.ts
+ *
+ * This issue has a link to the above file:
+ * https://github.com/material-foundation/material-color-utilities/issues/98
+ *
+ * This YouTube video explains the dynamic scheme:
+ * https://youtu.be/vnDhq8W98O4?t=648
+ *
+ * This blog post explains the surface colors:
+ * https://material.io/blog/tone-based-surface-color-m3
+ *
+ */
+
 import { computed, toValue } from "vue";
 import type { MaybeRefOrGetter } from "vue";
 import {
@@ -8,28 +29,30 @@ import {
   DynamicColor,
 } from "@material/material-color-utilities";
 import type { DynamicScheme } from "@material/material-color-utilities";
+import type { SnakeCase } from "type-fest";
 
-/**
- * The code to generate Dynamic Color of Material Design:
- * https://m3.material.io/styles/color/dynamic-color/
- *
- * It uses material-color-utilities:
- * https://github.com/material-foundation/material-color-utilities
- *
- * The code is based on dynamic_color_test.ts:
- * https://github.com/material-foundation/material-color-utilities/blob/ce99247b6ba/typescript/dynamiccolor/dynamic_color_test.ts
- *
- * This issue has a link to the above file:
- * https://github.com/material-foundation/material-color-utilities/issues/98
- *
- *
- * This YouTube video explains the dynamic scheme:
- * https://youtu.be/vnDhq8W98O4?t=648
- *
- * This blog post explains the surface colors:
- * https://material.io/blog/tone-based-surface-color-m3
- *
- */
+// An array of DynamicColor instances. They are properties of
+// MaterialDynamicColors whose value types are DynamicColor, i.e.,
+// [MaterialDynamicColors.background, MaterialDynamicColors.onBackground, ...]
+const dynamicColors = Object.values(MaterialDynamicColors).filter(
+  (color): color is DynamicColor => color instanceof DynamicColor
+);
+
+// Literal type of the color names in camelCase, e.g., "background", "onBackground", ...
+// They are the keys of MaterialDynamicColors whose value types are DynamicColor.
+export type ColorNameCamelCase = {
+  [K in keyof typeof MaterialDynamicColors]: typeof MaterialDynamicColors[K] extends DynamicColor
+    ? K
+    : never;
+}[keyof typeof MaterialDynamicColors];
+
+// Literal type of the color names in snake_case, e.g., "background", "on_background", ...
+// DynamicColor.name is in snake_case.
+export type ColorNameSnakeCase = SnakeCase<ColorNameCamelCase>;
+
+export type DynamicColors = {
+  [K in ColorNameSnakeCase]: number;
+};
 
 export function useDynamicColors(
   sourceColor: MaybeRefOrGetter<string>,
@@ -60,24 +83,6 @@ function useDynamicScheme(
 
   return scheme;
 }
-
-// An array of DynamicColor.
-// e.g., [MaterialDynamicColors.background, MaterialDynamicColors.onBackground, ...]
-const dynamicColors = Object.values(MaterialDynamicColors).filter(
-  (color): color is DynamicColor => color instanceof DynamicColor
-);
-
-// Literal type of the keys of MaterialDynamicColors whose value types are DynamicColor.
-// In other words, the names of the dynamic colors, e.g., "background", "onBackground", ..
-type ColorNames = {
-  [K in keyof typeof MaterialDynamicColors]: typeof MaterialDynamicColors[K] extends DynamicColor
-    ? K
-    : never;
-}[keyof typeof MaterialDynamicColors];
-
-export type DynamicColors = {
-  [K in ColorNames]: number;
-};
 
 const schemeToDynamicColors = (scheme: DynamicScheme): DynamicColors =>
   // @ts-ignore
