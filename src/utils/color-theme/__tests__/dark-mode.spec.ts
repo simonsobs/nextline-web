9e06fb6185ddca40801a9bb8cb158c40f8dc9015
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-24 15:12:01 -0400

    add a test

---
+++ src/utils/color-theme/__tests__/dark-mode.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { useDarkMode } from "../dark-mode";
+
+describe("toggleDark", () => {
+  it("should change isDark", () => {
+    const { isDark, toggleDark } = useDarkMode();
+    isDark.value = false;
+    toggleDark();
+    expect(isDark.value).toBe(true);
+  });
+});
