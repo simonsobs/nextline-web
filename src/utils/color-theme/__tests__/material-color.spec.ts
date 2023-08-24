8f009367d94b2320c3f17286d11ec5eb1c8ff4fd
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-24 15:57:29 -0400

    add material-color.spec.ts

---
+++ src/utils/color-theme/__tests__/material-color.spec.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+import { ref } from "vue";
+import { useDynamicColors } from "../material-color";
+
+describe("useDynamicColors", () => {
+  it("returns the correct colors", () => {
+    const sourceColor = ref("#2196f3");
+    const dark = ref(false);
+    const contrastLevel = ref(0.0);
+    const { colors } = useDynamicColors(sourceColor, dark, contrastLevel);
+    expect(colors.value).toHaveProperty("primary");
+  });
+});
