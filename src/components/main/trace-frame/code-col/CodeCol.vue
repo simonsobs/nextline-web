4fa89144c07b7ed59a4d9101fb83ef011c6b1013
Tai Sakuma <tai.sakuma@gmail.com>
2023-08-31 12:13:21 -0400

    split CodeCol.vue into Editor.vue and CodeCol.vue

--- src/components/main/trace-frame/code-col/CodeCol.vue
+++ src/components/main/trace-frame/code-col/CodeCol.vue
@@ -1,13 +1,20 @@
 <template>
-  <div ref="refEditor" style="height: 100%;"></div>
+  <editor
+    :key="fileName"
+    :state="state"
+    :source="source"
+    v-if="source !== undefined"
+  >
+  </editor>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, nextTick, toRefs, toValue } from "vue";
+import { computed, toRefs } from "vue";
 
 import { PromptingData } from "@/graphql/codegen/generated";
 
-import { useMonacoEditor, useScroll, useMarkCurrentLine } from "./monaco-editor";
+import Editor from "./Editor.vue";
+
 import { useSource } from "./source";
 
 interface Props {
@@ -15,77 +22,7 @@ interface Props {
 }
 
 const props = defineProps<Props>();
-
 const { state } = toRefs(props);
-
-const refEditor = ref<HTMLElement>();
-
 const fileName = computed(() => state.value.fileName);
 const { source } = useSource(fileName);
-
-const { editor, model } = useMonacoEditor(refEditor, source);
-const { scroll } = useScroll(editor);
-const { markCurrentLine } = useMarkCurrentLine(editor);
-
-const lineNo = computed(() => state.value.lineNo);
-const prompting = computed(() => state.value.prompting);
-
-const className = computed(() =>
-  prompting.value ? "currentLineContent" : "currentLineContentDim"
-);
-const glyphMarginClassName = computed(() =>
-  prompting.value ? "currentLineMargin" : "currentLineMarginDim"
-);
-
-function onUpdated() {
-  scroll(lineNo.value);
-  markCurrentLine(lineNo.value, className.value, glyphMarginClassName.value);
-}
-
-watch(
-  source,
-  (val) => {
-    if (source.value === undefined) return;
-    model.setValue(source.value);
-    nextTick(() => {
-      onUpdated();
-    });
-  },
-  { immediate: true }
-);
-
-watch(
-  state,
-  (val) => {
-    if (source.value === undefined) return;
-    nextTick(() => {
-      onUpdated();
-    });
-  },
-  { immediate: true }
-);
 </script>
-
-<style scoped>
-:deep(.monaco-editor .cursors-layer > .cursor) {
-  display: none !important;
-}
-:deep(.monaco-editor .currentLineContent) {
-  background: rgb(var(--v-theme-surface-variant));
-}
-:deep(.monaco-editor .currentLineContentDim) {
-  background: rgb(var(--v-theme-surface-container-highest));
-}
-:deep(.monaco-editor .currentLineMargin::before) {
-  color: rgb(var(--v-theme-primary));
-  content: "⮕";
-  font-family: "Noto Sans JP", sans-serif;
-  font-size: 24px;
-}
-:deep(.monaco-editor .currentLineMarginDim::before) {
-  color: rgb(var(--v-theme-surface-container-highest));
-  content: "⮕";
-  font-family: "Noto Sans JP", sans-serif;
-  font-size: 24px;
-}
-</style>
---
+++ src/components/main/trace-frame/code-col/Editor.vue
@@ -0,0 +1,94 @@
+<template>
+  <div ref="refEditor" style="height: 100%"></div>
+</template>
+
+<script setup lang="ts">
+import {
+  ref,
+  computed,
+  watch,
+  watchEffect,
+  nextTick,
+  toRefs,
+  toValue,
+} from "vue";
+
+import { PromptingData } from "@/graphql/codegen/generated";
+
+import {
+  useMonacoEditor,
+  useScroll,
+  useMarkCurrentLine,
+} from "./monaco-editor";
+
+interface Props {
+  state: PromptingData;
+  source: string;
+}
+
+const props = defineProps<Props>();
+
+const { state, source } = toRefs(props);
+
+const refEditor = ref<HTMLElement>();
+
+const { editor, model } = useMonacoEditor(refEditor, source);
+const { scroll } = useScroll(editor);
+const { markCurrentLine } = useMarkCurrentLine(editor);
+
+const lineNo = computed(() => state.value.lineNo);
+const prompting = computed(() => state.value.prompting);
+
+const className = computed(() =>
+  prompting.value ? "currentLineContent" : "currentLineContentDim"
+);
+const glyphMarginClassName = computed(() =>
+  prompting.value ? "currentLineMargin" : "currentLineMarginDim"
+);
+
+function onUpdated() {
+  scroll(lineNo.value);
+  markCurrentLine(lineNo.value, className.value, glyphMarginClassName.value);
+}
+
+watchEffect(() => {
+  // This is unnecessary as the source doesn't change for a given instance.
+  // When the source changes, a new instance is created.
+  model.setValue(toValue(source));
+});
+
+watch(
+  [source, state, editor],
+  (val) => {
+    nextTick(() => {
+      onUpdated();
+    });
+  },
+  { immediate: true }
+);
+
+</script>
+
+<style scoped>
+:deep(.monaco-editor .cursors-layer > .cursor) {
+  display: none !important;
+}
+:deep(.monaco-editor .currentLineContent) {
+  background: rgb(var(--v-theme-surface-variant));
+}
+:deep(.monaco-editor .currentLineContentDim) {
+  background: rgb(var(--v-theme-surface-container-highest));
+}
+:deep(.monaco-editor .currentLineMargin::before) {
+  color: rgb(var(--v-theme-primary));
+  content: "⮕";
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 24px;
+}
+:deep(.monaco-editor .currentLineMarginDim::before) {
+  color: rgb(var(--v-theme-surface-container-highest));
+  content: "⮕";
+  font-family: "Noto Sans JP", sans-serif;
+  font-size: 24px;
+}
+</style>
