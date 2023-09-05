3ebe8e3d82045c65b00df1565291dc211609d626
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-05 15:41:31 -0400

    copy doc from material-colors.ts to dynamic-colors.ts
    material-colors.ts is to be deleted.

--- src/utils/dynamic-color/dynamic-colors.ts
+++ src/utils/dynamic-color/dynamic-colors.ts
@@ -1,3 +1,24 @@
+/**
+ * The code to generate Dynamic Color of Material Design:
+ * https://m3.material.io/styles/color/dynamic-color/
+ *
+ * It uses material-color-utilities:
+ * https://github.com/material-foundation/material-color-utilities
+ *
+ * The code was originally based on dynamic_color_test.ts:
+ * https://github.com/material-foundation/material-color-utilities/blob/ce99247b6ba/typescript/dynamiccolor/dynamic_color_test.ts
+ *
+ * This issue has a link to the above file:
+ * https://github.com/material-foundation/material-color-utilities/issues/98
+ *
+ * This YouTube video explains the dynamic scheme:
+ * https://youtu.be/vnDhq8W98O4?t=648
+ *
+ * This blog post explains the surface colors:
+ * https://material.io/blog/tone-based-surface-color-m3
+ *
+ */
+
 import { computed } from "vue";
 import type { MaybeRef } from "vue";
 import { hexFromArgb } from "@material/material-color-utilities";
