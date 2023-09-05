4af7c341faae0e31e683c24f1c778e174b0e38fc
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-05 15:30:21 -0400

    update tests

--- src/utils/dynamic-color/__tests__/dynamic-colors.spec.ts
+++ src/utils/dynamic-color/__tests__/dynamic-colors.spec.ts
@@ -2,8 +2,18 @@ import { describe, it, expect } from "vitest";
 import { useDynamicColors } from "..";
 
 describe("useDynamicColors", () => {
-  it("returns scheme", () => {
+  it("returns colors", () => {
     const { colors } = useDynamicColors();
-	expect(colors.value).toMatchSnapshot();
+    expect(colors.value).toMatchSnapshot();
+  });
+
+  it("colors are reactive", () => {
+    const { colors, sourceColorHex } = useDynamicColors({
+      sourceColorHex: "#F44336",
+    });
+    const colors1 = colors.value;
+    sourceColorHex.value = "#673AB7";
+    const colors2 = colors.value;
+    expect(colors1).not.toEqual(colors2);
   });
 });
