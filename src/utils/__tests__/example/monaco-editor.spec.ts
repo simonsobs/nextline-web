382c0f5cdeed31d17a6a503ebe682170d7d5ddfe
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-06 16:48:54 -0400

    add monaco-editor.spec.ts

---
+++ src/utils/__tests__/example/monaco-editor.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import * as monaco from "monaco-editor";
+
+describe("monaco-editor", () => {
+  // Note: The test fails at the initial run with
+  // TypeError: Cannot read properties of null (reading 'webkitBackingStorePixelRatio')
+  // However, it will pass at rerun (press "f").
+  it("create", () => {
+    const ele = document.createElement("div");
+    const model = monaco.editor.createModel("hello world", "text/plain");
+    const editor = monaco.editor.create(ele, { model });
+    expect(editor.getValue()).toBe("hello world");
+  });
+});
