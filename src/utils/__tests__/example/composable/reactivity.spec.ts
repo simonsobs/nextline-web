8faa3f6cf06420067f73eb0c94811ecced2b45d2
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-06 12:51:00 -0400

    add comments to reactivity.spec.ts

--- src/utils/__tests__/example/composable/reactivity.spec.ts
+++ src/utils/__tests__/example/composable/reactivity.spec.ts
@@ -3,18 +3,32 @@ import { useTitle } from "@vueuse/core";
 import { ref } from "vue";
 import type { MaybeRef } from "vue";
 
-// Test the argument of useTitle() is reactive
 test("useTitle() should be reactive", () => {
-  const title = ref("Hello World");
-  const t = useTitle(title);
+  
+  // Initialize useTitle() with a ref
+  const initTitle = ref("Hello World");
+  const title = useTitle(initTitle);
+
+  expect(initTitle.value).toBe("Hello World");
   expect(title.value).toBe("Hello World");
-  title.value = "Hello Vue";
+
+  // title is still reactive to initTitle
+  initTitle.value = "Hello Vue";
+  expect(initTitle.value).toBe("Hello Vue");
   expect(title.value).toBe("Hello Vue");
-  t.value = "New Title";
-  expect(t.value).toBe("New Title");
+
+  // initTitle is also reactive to title
+  title.value = "New Title";
   expect(title.value).toBe("New Title");
+  expect(initTitle.value).toBe("New Title");
+  
+  // In fact, they are the same object
+  expect(title === initTitle).toBe(true);
 });
 
+// How to implement above reactivity.
+// Return ref() of the initial value.
+// ref() returns the same object if the argument is a ref.
 function useScratch(newScratch: MaybeRef<string>) {
   const scratch = ref(newScratch);
   return scratch;
@@ -29,6 +43,7 @@ test("useScratch() non ref arg", () => {
 test("useScratch() should be reactive", () => {
   const newScratch = ref("Hello World");
   const scratch = useScratch(newScratch);
+  expect(scratch === newScratch).toBe(true);
   expect(scratch.value).toBe("Hello World");
   scratch.value = "Hello Vue";
   expect(scratch.value).toBe("Hello Vue");
