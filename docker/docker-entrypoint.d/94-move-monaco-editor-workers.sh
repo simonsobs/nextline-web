58d447f61a75c0c54e5a17a173116292c06620b2
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-25 10:37:43 -0400

    rewrite 94-move-monaco-editor-workers.sh

--- docker/docker-entrypoint.d/94-move-monaco-editor-workers.sh
+++ docker/docker-entrypoint.d/94-move-monaco-editor-workers.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-source $(dirname -- "$0")/envvar.sh;
+source $(dirname -- "$0")/envvar.sh
 
 FILE=$(basename $0)
 
@@ -12,22 +12,18 @@ BASE_NAME=monacoeditorwork
 SOURCE=$(echo ${SITE_DIR}/${PUBLIC_PATH_PLACEHOLDER}/${BASE_NAME} | tr -s /)
 DEST=$(echo ${SITE_DIR}/${BASE_NAME} | tr -s /)
 
-if [[ ! -d ${SOURCE} ]]
-then
-    echo "$FILE: the source dir doesn't exist: $SOURCE"
-    exit 0
-fi
+[ -d "${SOURCE}" ] || {
+    echo "Error: ${SOURCE} doesn't exist!"
+    exit 1
+}
 
-if [[ $SOURCE == $DEST ]]
-then
+[[ $SOURCE == $DEST ]] && {
     echo "$FILE: the source and destination are the same: $SOURCE"
     exit 0
-fi
+}
 
-command="rm -rf $DEST"
-echo + $command
-eval $command
-
-command="mv $SOURCE $DEST"
-echo + $command
-eval $command
+(
+    set -x
+    rm -rf $DEST
+    mv $SOURCE $DEST
+)
