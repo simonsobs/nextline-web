ee60c990a77d3fd21ddd5603619c1920b1f050fb
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-29 16:29:17 -0400

    copy RefreshButton.vue from acondbw

---
+++ src/views/db/RefreshButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-tooltip>
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="props"
+        variant="text"
+        :disabled="disabled"
+        icon
+        @click="emit('refresh')"
+      >
+        <v-icon icon="mdi-refresh"></v-icon>
+      </v-btn>
+    </template>
+    <span>Refresh</span>
+  </v-tooltip>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  disabled?: boolean;
+}
+type Emits = {
+  "refresh": [];
+}
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+</script>
