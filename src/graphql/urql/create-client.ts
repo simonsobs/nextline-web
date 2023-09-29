6ae8847379f5caed7b245b7568464a3640d80177
Tai Sakuma <tai.sakuma@gmail.com>
2023-09-29 18:53:50 -0400

    clean code

--- src/graphql/urql/create-client.ts
+++ src/graphql/urql/create-client.ts
@@ -4,34 +4,31 @@ import {
   fetchExchange,
   subscriptionExchange,
 } from "@urql/vue";
+import type { SubscriptionForwarder } from "@urql/vue";
 import { createClient as createWSClient } from "graphql-ws";
 
 export function createClient(url: string) {
   // WebSocket endpoint. "ws://" for "http://" and "wss://" for "https://"
   const wsEndpoint = url.replace(/^http/i, "ws");
 
-  const wsClient = createWSClient({
-    url: wsEndpoint,
-  });
+  const wsClient = createWSClient({ url: wsEndpoint });
+
+  const forwardSubscription: SubscriptionForwarder = (request) => {
+    const input = { ...request, query: request.query || "" };
+    return {
+      subscribe(sink) {
+        const unsubscribe = wsClient.subscribe(input, sink);
+        return { unsubscribe };
+      },
+    };
+  };
+
+  const _subscriptionExchange = subscriptionExchange({ forwardSubscription });
 
   const client = createUrqlClient({
     url: url,
     requestPolicy: "network-only",
-    exchanges: [
-      cacheExchange,
-      fetchExchange,
-      subscriptionExchange({
-        forwardSubscription(request) {
-          const input = { ...request, query: request.query || "" };
-          return {
-            subscribe(sink) {
-              const unsubscribe = wsClient.subscribe(input, sink);
-              return { unsubscribe };
-            },
-          };
-        },
-      }),
-    ],
+    exchanges: [cacheExchange, fetchExchange, _subscriptionExchange],
   });
 
   return client;
