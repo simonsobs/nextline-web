40fdb2a2acd569737f88c65c5b47aacb54ff6489
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-03 16:08:36 -0400

    remove content from ScratchView.vue

--- src/views/scratch/ScratchView.vue
+++ src/views/scratch/ScratchView.vue
@@ -1,127 +1,16 @@
 <template>
   <div class="g-container">
-    <v-layout>
-      <v-card flat min-width="400px" color="background-lighten-4">
-        <v-system-bar color="primary" dark style="position: static">
-          <span> System bar </span>
-          <v-spacer></v-spacer>
-          <v-icon>mdi-minus</v-icon>
-          <v-icon>mdi-checkbox-blank-outline</v-icon>
-          <v-icon>mdi-close</v-icon>
-        </v-system-bar>
-        <v-toolbar flat color="primary-lighten-2" dark extension-height="36px">
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          <v-toolbar-title> Tool bar </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-          <template v-slot:extension>
-            <v-tabs
-              v-model="tab"
-              align-with-title
-              height="36px"
-              hide-slider
-              active-class="bg-background-lighten-4 text-primary"
-            >
-              <v-tab
-                v-for="item in items"
-                :key="item"
-                :value="item"
-                :ripple="false"
-                class="text-none"
-              >
-                {{ item }}
-              </v-tab>
-            </v-tabs>
-          </template>
-        </v-toolbar>
-        <v-window v-model="tab">
-          <v-window-item
-            v-for="item in items"
-            :key="item"
-            :value="item"
-            :transition="false"
-            :reverse-transition="false"
-          >
-            <v-card-text>
-              <pre>{{ item }}</pre>
-            </v-card-text>
-          </v-window-item>
-        </v-window>
-      </v-card>
-    </v-layout>
-    <v-layout>
-      <v-card flat min-width="400px" color="background-lighten-4">
-        <v-system-bar color="primary" dark style="position: static;">
-          <span> System bar </span>
-          <v-spacer></v-spacer>
-          <v-icon>mdi-minus</v-icon>
-          <v-icon>mdi-checkbox-blank-outline</v-icon>
-          <v-icon>mdi-close</v-icon>
-        </v-system-bar>
-        <v-tabs
-          v-model="tab"
-          align-with-title
-          height="36px"
-          hide-slider
-          dark
-          background-color="primary-lighten-2"
-          active-class="bg-background-lighten-4 text-primary"
-        >
-          <v-tab
-            v-for="item in items"
-            :key="item"
-            :value="item"
-            :ripple="false"
-            class="text-none"
-          >
-            <v-badge content="1" color="error" inline>
-              {{ item }}
-            </v-badge>
-          </v-tab>
-        </v-tabs>
-        <v-window v-model="tab">
-          <v-window-item
-            v-for="item in items"
-            :key="item"
-            :value="item"
-            :transition="false"
-            :reverse-transition="false"
-          >
-            <v-card-text>
-              <pre>{{ item }}</pre>
-            </v-card-text>
-          </v-window-item>
-        </v-window>
-      </v-card>
-    </v-layout>
+    Scratch
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const tab = ref<any>(null);
-const items = ref(["one", "two", "three"]);
-const text = ref(
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-);
 </script>
 
 <style scoped>
 .g-container {
   display: grid;
   height: 100%;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-  gap: 6px;
-  justify-items: center;
-  align-items: start;
-  align-content: space-around;
-}
-</style>
-
-<style>
-.v-tab::before {
-  background-color: inherit;
+  place-items: center;
 }
 </style>
