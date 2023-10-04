5379c6bdab18a6e48c64af1dc9f5e8ca25564126
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-04 17:31:43 -0400

    add comments

--- src/utils/config/ProvideConfigExe.vue
+++ src/utils/config/ProvideConfigExe.vue
@@ -3,11 +3,16 @@
 </template>
 
 <script setup lang="ts">
+/**
+ * Load config asynchronously and provide it to the child components.
+ */
 import type { ComputedRef } from "vue";
 import { useLoadConfig } from "@/utils/config";
 import { useProvideConfig, Config } from "@/utils/config";
 const { error, config } = await useLoadConfig();
-if(error.value)  throw error.value; 
-if(!config.value) throw new Error("Config is null");
+if (error.value) throw error.value;
+if (!config.value) throw new Error("Config is null");
 useProvideConfig(config as ComputedRef<Config>);
+// await new Promise((resolve) => setTimeout(resolve, 1000));
+// throw new Error("Test");
 </script>
