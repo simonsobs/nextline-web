e3d96e105a83bc21424a50388a4c055409fad82b
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-04 10:18:27 -0400

    update ScratchView.vue

---
+++ src/components/scratch/A.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Scratch A</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+async function fetch() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));	
+  // throw new Error("This is an error");
+  return "Do eiusmod reprehenderit voluptate do labore veniam voluptate anim. ";
+}
+
+const text = await fetch();
+</script>
--- src/views/scratch/ScratchView.vue
+++ src/views/scratch/ScratchView.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="g-container">
-    Scratch
+    <v-alert v-if="error" variant="tonal" type="error">
+      {{ error }}
+    </v-alert>
+    <Suspense v-else>
+      <component-a></component-a>
+      <template #fallback>
+        <v-skeleton-loader
+          type="paragraph"
+          min-width="360px"
+        ></v-skeleton-loader>
+      </template>
+    </Suspense>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, onErrorCaptured } from "vue";
+import ComponentA from "@/components/scratch/A.vue";
+
+const error = ref<Error>();
+
+onErrorCaptured((e, instance, info) => {
+  console.error(e, instance, info);
+  error.value = e;
+  return false;
+});
 </script>
 
 <style scoped>
