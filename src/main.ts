4e309af0eae960c31db5af4a5e5b765ad694efbb
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-11 10:30:42 -0400

    add errorHandler to app

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,16 @@ import SuspenseC from "@/components/SuspenseC.vue";
 
 // Types of global components are exported from src/global-components.d.ts
 
-createApp({ render: () => h(App) })
+const app = createApp({ render: () => h(App) });
+
+app.config.errorHandler = (err, vm, info) => {
+  // Source of errors captured here are listed in
+  // https://vuejs.org/api/application.html#app-config-errorhandler
+  // Unhandled promise rejections are not captured here.
+  console.error("Global Error:", "\nInstance:", vm, "\nInfo:", info, "\n", err);
+};
+
+app
   .component("DevToolCheckboxes", DevToolCheckboxes)
   .component("SuspenseC", SuspenseC)
   .use(pinia)
