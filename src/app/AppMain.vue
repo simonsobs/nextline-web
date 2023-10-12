3ebcde39d62274f646f6493482ddb363d551c91f
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-12 10:41:23 -0400

    add a banner above the app bar

--- src/app/AppMain.vue
+++ src/app/AppMain.vue
@@ -1,21 +1,29 @@
 <template>
-  <navigation-drawer v-model="drawer"></navigation-drawer>
-  <app-bar>
-    <template v-slot:prepend>
-      <v-app-bar-nav-icon @click="toggleDrawer()" v-if="mobile"> </v-app-bar-nav-icon>
-    </template>
-  </app-bar>
-  <v-main>
-    <router-view v-slot="{ Component, route }">
-      <template v-if="Component">
-        <v-fade-transition leave-absolute>
-          <keep-alive>
-            <component :key="route.fullPath" :is="Component"></component>
-          </keep-alive>
-        </v-fade-transition>
-      </template>
-    </router-view>
-  </v-main>
+  <div class="app-main">
+    <div class="banner">
+      <banner></banner>
+    </div>
+    <div class="app">
+      <navigation-drawer v-model="drawer"></navigation-drawer>
+      <app-bar :absolute="true">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click="toggleDrawer()" v-if="mobile">
+          </v-app-bar-nav-icon>
+        </template>
+      </app-bar>
+      <v-main>
+        <router-view v-slot="{ Component, route }">
+          <template v-if="Component">
+            <v-fade-transition leave-absolute>
+              <keep-alive>
+                <component :key="route.fullPath" :is="Component"></component>
+              </keep-alive>
+            </v-fade-transition>
+          </template>
+        </router-view>
+      </v-main>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -26,6 +34,7 @@ import { useColorTheme } from "@/utils/color-theme";
 import { useSetTitle } from "./set-title";
 import { useDrawer } from "./drawer";
 
+import Banner from "./Banner.vue";
 import NavigationDrawer from "./NavigationDrawer.vue";
 import AppBar from "./AppBar.vue";
 
@@ -41,8 +50,27 @@ globalThis.addEventListener("unhandledrejection", (event) => {
 </script>
 
 <style scoped>
+.app-main {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: min-content minmax(240px, 1fr);
+  grid-template-areas: "banner" "app";
+}
+
+.banner {
+  grid-area: banner;
+}
+
+.app {
+  grid-area: app;
+  position: relative;
+  height: 100%;
+}
+
 .v-main {
-  height: calc(100% - var(--v-layout-top));
+  height: 100%;
+  /* height: calc(100% - var(--v-layout-top)); */
   /* --v-layout-top is the height of the app bar */
   overflow-y: scroll;
 }
---
+++ src/app/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <template v-if="on">
+    <v-banner lines="one" icon="mdi-alert-outline">
+      <template #text> Banner </template>
+      <template #actions>
+        <v-btn> Action </v-btn>
+      </template>
+    </v-banner>
+  </template>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const on = ref(false);
+</script>
