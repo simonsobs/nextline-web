38094ca730c2b2a02683497cd831335c3ee4689d
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-13 09:29:19 -0400

    rewrite SubscribeRunNo.vue with useSubscribeRunNo()

--- src/components/main/run-frame/SubscribeRunNo.vue
+++ src/components/main/run-frame/SubscribeRunNo.vue
@@ -3,18 +3,8 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import {
-  useQRunNoQuery,
-  useRunNoSubscription,
-} from "@/graphql/codegen/generated";
-
-const query = await useQRunNoQuery();
-const subscription = useRunNoSubscription();
-
-const runNo = computed(
-  () => subscription.data?.value?.runNo || query.data?.value?.runNo
-);
-
-// await new Promise((resolve) => setTimeout(resolve, 1000));
+import { useSubscribeRunNo } from "@/api";
+const runNoSubscription = useSubscribeRunNo();
+const { runNo } = runNoSubscription;
+await runNoSubscription;
 </script>
