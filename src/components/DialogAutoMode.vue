d4623e899d01b95d39ccd24c116166600734ea60
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-13 14:00:20 -0400

    update the layout of DialogAutoMode.vue

--- src/components/DialogAutoMode.vue
+++ src/components/DialogAutoMode.vue
@@ -1,43 +1,45 @@
 <template>
-  <v-card v-if="autoMode == undefined" class="pa-1" max-width="290px">
-    <v-alert
-      type="error"
-      variant="tonal"
-      title="Error"
-      text="Unable to determine auto mode state."
-    >
-    </v-alert>
-  </v-card>
-  <v-card v-else-if="autoMode" class="pa-1" max-width="290px">
-    <v-card-text>
-      <div class="text=h6 mb-1">
-        <p>
-          In auto mode, after each run, the next script is automatically loaded
-          from the scheduler and run.
-        </p>
-        <p style="margin-top: 1.5em">Turn off auto mode?</p>
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn variant="outlined" @click="turnOff"> turn off </v-btn>
-    </v-card-actions>
-  </v-card>
-  <v-card v-else class="pa-1" max-width="290px">
-    <v-card-text>
-      <div class="text=h6 mb-1">
-        <p>
-          In auto mode, after each run, the next script is automatically loaded
-          from the scheduler and run.
-        </p>
-        <p style="margin-top: 1.5em">Turn on auto mode?</p>
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn variant="outlined" @click="turnOn"> turn on </v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <v-card v-if="autoMode == undefined">
+      <v-alert
+        type="error"
+        variant="tonal"
+        title="Error"
+        text="Unable to determine auto mode state."
+      >
+      </v-alert>
+    </v-card>
+    <v-card v-else-if="autoMode">
+      <v-card-text>
+        <div class="text=h6 mb-1">
+          <p>
+            In auto mode, after each run, the next script is automatically loaded from
+            the scheduler and run.
+          </p>
+          <p style="margin-top: 1.5em">Turn off auto mode?</p>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn variant="outlined" @click="turnOff"> turn off </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card v-else>
+      <v-card-text>
+        <div class="text=h6 mb-1">
+          <p>
+            In auto mode, after each run, the next script is automatically loaded from
+            the scheduler and run.
+          </p>
+          <p style="margin-top: 1.5em">Turn on auto mode?</p>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn variant="outlined" @click="turnOn"> turn on </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -53,5 +55,12 @@ import { useSubscribeScheduleAutoMode } from "@/api";
 const subscription = useSubscribeScheduleAutoMode();
 const { autoMode } = subscription;
 
-await subscription
+await subscription;
 </script>
+
+<style scoped>
+:deep(.v-card) {
+  width: 290px;
+  padding: 4px;
+}
+</style>
