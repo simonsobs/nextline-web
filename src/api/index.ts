5eaa1f3f9f62b81b49540676c14d36aedbe4fc31
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-13 12:41:50 -0400

    correct a file name

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
 export * from "./use-run-no-subscription";
 export * from "./use-state-subscription";
-export * from "./use-trace_ids-subscription copy";
+export * from "./use-trace_ids-subscription";
 export * from "./use-continuous-enabled-subscription";
