fc0f3b9dd9e242ac7268c566a98a78bb0a12e344
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-13 14:08:24 -0400

    change colors

--- src/components/main/run-frame/top-bar/ActionRunning.vue
+++ src/components/main/run-frame/top-bar/ActionRunning.vue
@@ -1,30 +1,30 @@
 <template>
   <v-spacer></v-spacer>
-  <v-btn variant="outlined" prepend-icon="mdi-close" @click="executeInterrupt">
+  <v-btn
+    variant="outlined"
+    color="error"
+    prepend-icon="mdi-close"
+    @click="executeInterrupt"
+  >
     interrupt
   </v-btn>
   <v-menu offset-y>
     <template v-slot:activator="{ props }">
-      <v-btn
-        variant="text"
-        v-bind="props"
-        icon="mdi-dots-horizontal"
-        density="compact"
-      >
+      <v-btn variant="text" v-bind="props" icon="mdi-dots-horizontal" density="compact">
       </v-btn>
     </template>
     <v-list>
-      <v-list-item @click="executeTerminate({})">
+      <v-list-item @click="executeTerminate({})" class="text-error">
         <template v-slot:prepend>
           <v-icon> mdi-close-octagon-outline </v-icon>
         </template>
-        terminate
+        Terminate
       </v-list-item>
-      <v-list-item @click="executeKill({})">
+      <v-list-item @click="executeKill({})" class="text-error">
         <template v-slot:prepend>
           <v-icon> mdi-close-octagon </v-icon>
         </template>
-        kill
+        Kill
       </v-list-item>
     </v-list>
   </v-menu>
