5830a45c52aa1afd36576302c471bd3b853113fe
Tai Sakuma <tai.sakuma@gmail.com>
2023-10-13 09:21:44 -0400

    rewrite SpanRunNoState.vue with useContinuousEnabledSubscription()

--- src/components/main/run-frame/top-bar/SpanRunNoState.vue
+++ src/components/main/run-frame/top-bar/SpanRunNoState.vue
@@ -13,14 +13,11 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-
 import {
-  useQContinuousEnabledQuery,
-  useContinuousEnabledSubscription,
-} from "@/graphql/codegen/generated";
-
-import { useSubscribeRunNo, useSubscribeState } from "@/api";
+  useSubscribeRunNo,
+  useSubscribeState,
+  useSubscribeContinuousEnabled,
+} from "@/api";
 
 const runNoSubscription = useSubscribeRunNo();
 const { runNo } = runNoSubscription;
@@ -28,14 +25,12 @@ const { runNo } = runNoSubscription;
 const stateSubscription = useSubscribeState();
 const { state } = stateSubscription;
 
-const continuousEnabledQuery = useQContinuousEnabledQuery();
-const continuousEnabledSubscription = useContinuousEnabledSubscription();
+const continuousEnabledSubscription = useSubscribeContinuousEnabled();
+const { continuousEnabled } = continuousEnabledSubscription;
 
-const continuousEnabled = computed(
-  () =>
-    continuousEnabledSubscription.data?.value?.continuousEnabled ||
-    continuousEnabledQuery.data?.value?.continuousEnabled
-);
-
-await Promise.all([runNoSubscription, stateSubscription]);
+await Promise.all([
+  runNoSubscription,
+  stateSubscription,
+  continuousEnabledSubscription,
+]);
 </script>
