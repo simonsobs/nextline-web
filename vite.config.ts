b6889dc9694d03ab5ca88428d947b0ebbb239ad9
Tai Sakuma <tai.sakuma@gmail.com>
2024-02-12 14:21:29 -0500

    Fix issue with vite-plugin-monaco-editor

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,23 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import loadVersion from "vite-plugin-package-version";
 import graphql from "@rollup/plugin-graphql";
-import monacoEditorPlugin from "vite-plugin-monaco-editor";
+// import monacoEditorPlugin from "vite-plugin-monaco-editor";
+import monacoEditorPluginModule from "vite-plugin-monaco-editor";
+
+// A temporary fix for the issue on vite-plugin-monaco-editor:
+// https://github.com/vdesjs/vite-plugin-monaco-editor/issues/21#issuecomment-1827562674
+const isObjectWithDefaultFunction = (
+  module: unknown
+): module is { default: typeof monacoEditorPluginModule } =>
+  module != null &&
+  typeof module === "object" &&
+  "default" in module &&
+  typeof module.default === "function";
+
+const monacoEditorPlugin = isObjectWithDefaultFunction(monacoEditorPluginModule)
+  ? monacoEditorPluginModule.default
+  : monacoEditorPluginModule;
+//
 
 export default ({ mode }) => {
   // loadEnv: https://stackoverflow.com/a/66389044/7309855
