e1409f4d86378b4c75ebae3b51be0a62f9d507cb
Tai Sakuma <tai.sakuma@gmail.com>
2024-02-19 12:02:53 -0500

    Load only a single run in the Run view

--- src/views/db/RunView.vue
+++ src/views/db/RunView.vue
@@ -2,7 +2,7 @@
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 
-import { useRunsQuery } from "@/graphql/codegen/generated";
+import { useRdbRunQuery } from "@/graphql/codegen/generated";
 
 import RunCard from "@/components/History/RunCard.vue";
 
@@ -14,48 +14,27 @@ const breadcrumb = computed(() => [
   { title: `Run ${runNo}`, to: { name: "run", params: { runNo } } },
 ]);
 
-const query = useRunsQuery();
+const queryResponse = useRdbRunQuery({ variables: { runNo } });
 
-type Query = typeof query;
-
-function readEdges(query: Query) {
-  const edgesAndNulls = query.data.value?.history.runs.edges;
-  if (!edgesAndNulls) return [];
-  return edgesAndNulls.flatMap((e) => (e ? [e] : []));
-}
-
-function readNodes(query: Query) {
-  return readEdges(query).flatMap((e) => (e.node ? e.node : []));
-}
-
-const nodes = computed(() => readNodes(query));
-const index = computed(() => nodes.value.findIndex((n) => n.runNo === runNo));
-const run = computed(() => nodes.value[index.value]);
-const toPrev = computed(() => {
-  const prevIndex = index.value - 1;
-  if (prevIndex < 0) return;
-  return { name: "run", params: { runNo: nodes.value[prevIndex].runNo } };
-});
-const toNext = computed(() => {
-  const nextIndex = index.value + 1;
-  if (nextIndex >= nodes.value.length) return;
-  return { name: "run", params: { runNo: nodes.value[nextIndex].runNo } };
-});
+const run = computed(() => queryResponse.data.value?.rdb.run);
 </script>
 
 <template>
   <div class="g-container">
     <div class="g-navi">
       <v-breadcrumbs :items="breadcrumb"> </v-breadcrumbs>
-      <span class="pr-6">
+      <!-- <span class="pr-6">
         <v-btn icon variant="plain" :to="toPrev" v-if="toPrev">
           <v-icon> mdi-arrow-left-bold </v-icon>
         </v-btn>
         <v-btn icon variant="plain" :to="toNext" v-if="toNext">
           <v-icon> mdi-arrow-right-bold </v-icon>
         </v-btn>
-      </span>
+      </span> -->
     </div>
+    <v-btn icon variant="plain" :to="{ name: 'runs' }">
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
     <run-card v-if="run" class="g-card" :run="run"></run-card>
   </div>
 </template>
@@ -68,8 +47,8 @@ const toNext = computed(() => {
   width: calc(100% - 2 * 12px);
   justify-content: center;
   grid-template-columns: minmax(min-content, 80%);
-  grid-template-rows: min-content 1fr;
-  grid-template-areas: "navi" "card";
+  grid-template-rows: min-content min-content 1fr;
+  grid-template-areas: "navi" "button" "card";
 }
 
 .g-navi {
