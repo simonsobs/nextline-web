e0b0669578bd08f14b50925c5f9ecd3efc2f88f4
Tai Sakuma <tai.sakuma@gmail.com>
2024-04-01 12:53:28 -0400

    Extract components from AutoModeButton.vue

--- src/components/auto-mode-button/AutoModeButton.vue
+++ src/components/auto-mode-button/AutoModeButton.vue
@@ -1,30 +1,15 @@
 <template>
   <v-card-actions>
-    <v-menu v-if="autoMode === undefined">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" variant="tonal" color="error"> Auto Mode: Unknown </v-btn>
-      </template>
-        <dialog-error></dialog-error>
-    </v-menu>
-    <v-menu v-else-if="autoMode">
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" variant="tonal" color="tertiary"> Auto Mode: On </v-btn>
-      </template>
-        <dialog-on></dialog-on>
-    </v-menu>
-    <v-menu v-else>
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props"> Auto Mode: Off </v-btn>
-      </template>
-      <dialog-off></dialog-off>
-    </v-menu>
+    <button-error v-if="autoMode === undefined"></button-error>
+    <button-on v-else-if="autoMode"></button-on>
+    <button-off v-else></button-off>
   </v-card-actions>
 </template>
 
 <script setup lang="ts">
-import DialogError from "./modes/error/Dialog.vue";
-import DialogOn from "./modes/on/Dialog.vue";
-import DialogOff from "./modes/off/Dialog.vue";
+import ButtonError from "./modes/error/Button.vue";
+import ButtonOn from "./modes/on/Button.vue";
+import ButtonOff from "./modes/off/Button.vue";
 import { useSubscribeScheduleAutoMode } from "@/api";
 const subscription = useSubscribeScheduleAutoMode();
 const { autoMode } = subscription;
---
+++ src/components/auto-mode-button/modes/off/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props"> Auto Mode: Off </v-btn>
+    </template>
+    <dialog-off></dialog-off>
+  </v-menu>
+</template>
+
+<script setup lang="ts">
+import DialogOff from "./Dialog.vue";
+</script>
