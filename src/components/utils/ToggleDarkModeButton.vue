057900a2283cc811b8da38bc84b930912e78cf4a
Tai Sakuma <tai.sakuma@gmail.com>
2024-04-03 10:39:50 -0400

    Split ToggleDarkModeButton.vue into two components

--- src/components/utils/ToggleDarkModeButton.vue
+++ src/components/utils/ToggleDarkModeButton.vue
@@ -1,13 +1,8 @@
 <template>
-  <v-tooltip location="top end">
-    <template #activator="{ props }">
-      <v-btn v-bind="props" variant="text" icon @click="toggleDark()">
-        <v-icon v-if="isDark" icon="mdi-weather-night"></v-icon>
-        <v-icon v-else icon="mdi-weather-sunny"></v-icon>
-      </v-btn>
-    </template>
-    <span>Toggle dark mode</span>
-  </v-tooltip>
+  <v-btn variant="text" icon @click="toggleDark()">
+    <v-icon v-if="isDark" icon="mdi-weather-night"></v-icon>
+    <v-icon v-else icon="mdi-weather-sunny"></v-icon>
+  </v-btn>
 </template>
 
 <script setup lang="ts">
