828ccb9395a35694141be82933bbdc19ee08396d
Tai Sakuma <tai.sakuma@gmail.com>
2024-04-18 10:20:30 -0400

    Extract ItemView.vue from ViewDialog.vue

---
+++ src/components/queue/ItemView.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-card-title>View Item</v-card-title>
+    <v-card-text>
+      <span class="font-weight-bold">Name:</span> {{ item?.name }}
+      <br />
+      <span class="font-weight-bold">Created at:</span> {{ item?.createdAt }}
+    </v-card-text>
+    <v-card-text>
+      <pre>{{ item?.script }}</pre>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="item = null">Close</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { defineModel } from "vue";
+import type { Item } from "./items";
+const item = defineModel<Item | null>();
+</script>
--- src/components/queue/ViewDialog.vue
+++ src/components/queue/ViewDialog.vue
@@ -1,24 +1,13 @@
 <template>
   <v-dialog v-model="show" persistent max-width="xl">
-    <v-card>
-      <v-card-title>View Item</v-card-title>
-      <v-card-text> 
-        <span class="font-weight-bold">Name:</span> {{ item?.name }}
-        <br />
-        <span class="font-weight-bold">Created at:</span> {{ item?.createdAt }}
-      </v-card-text>
-      <v-card-text>
-        <pre>{{ item?.script }}</pre>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn @click="item = null">Close</v-btn>
-      </v-card-actions>
-    </v-card>
+    <item-view v-model="item"> </item-view>
   </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref, defineModel, watchEffect } from "vue";
+
+import ItemView from "./ItemView.vue";
 
 import type { Item } from "./items";
 
