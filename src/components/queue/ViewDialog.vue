5c01f2966bf68945c895cde18fdf68953d86945f
Tai Sakuma <tai.sakuma@gmail.com>
2024-04-18 16:46:16 -0400

    Update the layout of ViewDialog.vue

--- src/components/queue/ViewDialog.vue
+++ src/components/queue/ViewDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <v-dialog v-model="show" :fullscreen="mobile" :transition="transition">
-    <v-sheet class="g-mobile pa-4" v-if="mobile">
-      <div class="g-top d-flex">
+    <v-sheet class="g-container pa-4" :class="{ 'g-mobile': mobile }">
+      <div class="g-top d-flex" v-if="mobile">
         <v-btn
           variant="text"
           size="small"
@@ -9,15 +9,10 @@
           @click="show = false"
         ></v-btn>
       </div>
-      <div class="g-content" style="block-size: 100%">
-        <item-view :item="item"> </item-view>
-      </div>
-    </v-sheet>
-    <v-sheet class="g-not-mobile pa-4" v-if="!mobile">
       <div class="g-content">
         <item-view :item="item"> </item-view>
       </div>
-      <div class="g-bottom d-flex">
+      <div class="g-bottom d-flex" v-if="!mobile">
         <v-spacer></v-spacer>
         <v-btn variant="text" @click="show = false">Close</v-btn>
       </div>
@@ -45,42 +40,32 @@ const show = defineModel<boolean>();
 </script>
 
 <style scoped>
-.g-mobile {
-  display: grid;
-  block-size: 100%;
-  inline-size: 100%;
-  row-gap: 24px;
-  grid-template-columns: minmax(100px, 1fr);
-  grid-template-rows: min-content minmax(0, 1fr);
-  grid-template-areas: "top" "content";
-}
-
-.g-mobile > .g-top {
-  grid-area: top;
-}
-
-.g-mobile > .g-content {
-  grid-area: content;
-  block-size: 100%;
-}
-
-.g-not-mobile {
+.g-container {
   display: grid;
   block-size: 70vh;
   inline-size: 100%;
+  max-inline-size: 960px;
+  margin: auto;
   row-gap: 24px;
   grid-template-columns: minmax(100px, 1fr);
-  grid-template-rows: minmax(0, 1fr) min-content;
-  grid-template-areas: "content" "bottom";
+  grid-template-rows: min-content minmax(0, 1fr) min-content;
+  grid-template-areas: "top" "content" "bottom";
 }
 
-.g-not-mobile > .g-content {
+.g-mobile {
+  block-size: 100%;
+}
+
+.g-top {
+  grid-area: top;
+}
+
+.g-content {
   grid-area: content;
   block-size: 100%;
 }
 
-.g-not-mobile > .g-bottom {
+.g-bottom {
   grid-area: bottom;
 }
-
 </style>
