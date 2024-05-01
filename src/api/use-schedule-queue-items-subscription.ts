f9ed2fbc01876fa60b4731e4ec0857c3ef327b9a
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-01 15:56:08 -0400

    Add useSubscribeScheduleQueueItems()

---
+++ src/api/use-schedule-queue-items-subscription.ts
@@ -0,0 +1,41 @@
+import { computed } from "vue";
+import type { ComputedRef } from "vue";
+import {
+  useQScheduleQueueItemsQuery,
+  useSScheduleQueueItemsSubscription,
+} from "@/graphql/codegen/generated";
+import type { ScheduleQueueItem } from "@/graphql/codegen/generated";
+
+interface _ScheduleQueueItemsSubscription {
+  items: ComputedRef<ScheduleQueueItem[] | undefined>;
+  error: ComputedRef<Error | undefined>;
+  subscription: ReturnType<typeof useSScheduleQueueItemsSubscription>;
+  query: ReturnType<typeof useQScheduleQueueItemsQuery>;
+}
+
+type ScheduleQueueItemsSubscription = _ScheduleQueueItemsSubscription &
+  PromiseLike<_ScheduleQueueItemsSubscription>;
+
+export function useSubscribeScheduleQueueItems(): ScheduleQueueItemsSubscription {
+  const query = useQScheduleQueueItemsQuery({ requestPolicy: "network-only" });
+  const subscription = useSScheduleQueueItemsSubscription();
+
+  const error = computed(() => subscription.error?.value || query.error?.value);
+
+  const items = computed(() =>
+    error.value
+      ? undefined
+      : subscription.data?.value?.scheduleQueueItems ||
+        query.data?.value?.schedule.queue.items
+  );
+
+  const ret = { items, error, subscription, query };
+
+  return {
+    ...ret,
+    async then(onFulfilled, onRejected) {
+      await query;
+      return Promise.resolve(ret).then(onFulfilled, onRejected);
+    },
+  };
+}
