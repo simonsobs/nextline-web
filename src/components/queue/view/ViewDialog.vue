e040cf3b15e2b26c655c2fa61dd255901af7a39d
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-29 12:04:00 -0400

    Add more menu to ViewDialog.vue

--- src/components/queue/view/ViewDialog.vue
+++ src/components/queue/view/ViewDialog.vue
@@ -8,12 +8,44 @@
           icon="mdi-close"
           @click="show = false"
         ></v-btn>
-        <v-spacer v-if="!mobile"></v-spacer>
+        <v-spacer></v-spacer>
         <v-btn
           variant="text"
           icon="mdi-trash-can-outline"
           @click="dialogConfirmDelete = true"
         ></v-btn>
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" variant="text" icon="mdi-dots-horizontal"></v-btn>
+          </template>
+          <v-list>
+            <v-List-subheader>Move (Reorder)</v-List-subheader>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon> mdi-arrow-up </v-icon>
+              </template>
+              To Top
+            </v-list-item>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon> mdi-arrow-up-thin </v-icon>
+              </template>
+              One Up
+            </v-list-item>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon> mdi-arrow-down-thin </v-icon>
+              </template>
+              One Down
+            </v-list-item>
+            <v-list-item>
+              <template v-slot:prepend>
+                <v-icon> mdi-arrow-down</v-icon>
+              </template>
+              To Bottom
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </div>
       <div class="g-content">
         <ContentFrame :item="item" :n-items="nItems"> </ContentFrame>
