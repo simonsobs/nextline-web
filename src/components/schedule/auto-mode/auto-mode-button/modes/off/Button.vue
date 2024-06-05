ac40a15ddca3c1b516f41d64e645d052abc03fba
Tai Sakuma <tai.sakuma@gmail.com>
2024-06-05 14:53:15 -0400

    Replace VMenu with VBottomSheet for mobile

--- src/components/schedule/auto-mode/auto-mode-button/modes/off/Button.vue
+++ src/components/schedule/auto-mode/auto-mode-button/modes/off/Button.vue
@@ -1,12 +1,30 @@
 <template>
-  <VMenu>
+  <Component :is="menuComponent">
     <template #activator="{ props }">
       <VBtn v-bind="props"> Auto Mode: Off </VBtn>
     </template>
-    <DialogOff></DialogOff>
-  </VMenu>
+    <div>
+      <Dialog class="dialog"></Dialog>
+    </div>
+  </Component>
 </template>
 
 <script setup lang="ts">
-import DialogOff from "./Dialog.vue";
+import { computed } from "vue";
+import { useDisplay } from "vuetify";
+import Dialog from "./Dialog.vue";
+const { mobile } = useDisplay();
+const menuComponent = computed(() => (mobile.value ? "VBottomSheet" : "VMenu"));
 </script>
+
+<style scoped>
+.v-bottom-sheet .dialog {
+  border-radius: 0 !important;
+  padding: 4px 4px 24px 4px;
+}
+
+.v-menu .dialog {
+  width: 290px;
+  padding: 4px;
+}
+</style>
--- src/components/schedule/auto-mode/auto-mode-button/modes/off/Dialog.vue
+++ src/components/schedule/auto-mode/auto-mode-button/modes/off/Dialog.vue
@@ -1,35 +1,33 @@
 <template>
-  <div>
-    <VCard>
-      <VCardText>
-        <div class="mb-1">
-          <p class="font-weight-medium">
-            The auto mode is currently <span class="font-italic"> off </span>.
-          </p>
-          <p class="mt-3">
-            If on, it automatically loads and runs the next script after each run
-            successfully ends.
-          </p>
-          <p class="mt-3">
-            The auto mode can load scripts from either the
-            <span class="font-italic"> scheduler </span> or the
-            <span class="font-italic"> queue </span>.
-          </p>
-          <p class="mt-3">Turn on auto mode?</p>
-        </div>
-      </VCardText>
-      <VCardActions>
-        <VSpacer></VSpacer>
-        <VBtn variant="outlined" @click="turnOnFromScheduler">
-          turn on from scheduler
-        </VBtn>
-      </VCardActions>
-      <VCardActions>
-        <VSpacer></VSpacer>
-        <VBtn variant="outlined" @click="turnOnFromQueue"> turn on from queue </VBtn>
-      </VCardActions>
-    </VCard>
-  </div>
+  <VCard>
+    <VCardText>
+      <div class="mb-1">
+        <p class="font-weight-medium">
+          The auto mode is currently <span class="font-italic"> off </span>.
+        </p>
+        <p class="mt-3">
+          If on, it automatically loads and runs the next script after each run
+          successfully ends.
+        </p>
+        <p class="mt-3">
+          The auto mode can load scripts from either the
+          <span class="font-italic"> scheduler </span> or the
+          <span class="font-italic"> queue </span>.
+        </p>
+        <p class="mt-3">Turn on auto mode?</p>
+      </div>
+    </VCardText>
+    <VCardActions>
+      <VBtn variant="outlined" @click="turnOnFromScheduler" class="flex-grow-1">
+        turn on from scheduler
+      </VBtn>
+    </VCardActions>
+    <VCardActions>
+      <VBtn variant="outlined" @click="turnOnFromQueue" class="flex-grow-1">
+        turn on from queue
+      </VBtn>
+    </VCardActions>
+  </VCard>
 </template>
 
 <script setup lang="ts">
@@ -45,10 +43,3 @@ async function turnOnFromQueue() {
   await changeMode({ mode: "queue" });
 }
 </script>
-
-<style scoped>
-:deep(.v-card) {
-  width: 290px;
-  padding: 4px;
-}
-</style>
