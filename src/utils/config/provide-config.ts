50348a1cc6c115187c23ba4428debfda4eb81b9a
Tai Sakuma <tai.sakuma@gmail.com>
2024-08-01 14:33:26 -0400

    Update `useProvideConfig()` to accept to a ref or getter of `null`

--- src/utils/config/provide-config.ts
+++ src/utils/config/provide-config.ts
@@ -2,15 +2,36 @@ import { ref, provide, watchEffect, toValue } from "vue";
 import type { MaybeRefOrGetter, InjectionKey, Ref } from "vue";
 
 import { injectionKey } from "./key";
-
 /**
- * Provide the config of type `T` to the child components.
- * In the child components, use `useConfig` to get the config.
+ * Provides a configuration of type `T` to child components.
+ *
+ * This function creates a reactive reference to the config and provides it
+ * to child components using Vue's provide/inject system.
+ *
+ * @template T The type of the configuration object
+ * @param The configuration, which can be:
+ *        - A value of type T
+ *        - A Ref<T | null>
+ *        - A getter function returning T or null
+ *
+ * @remarks
+ * In child components, use `useConfig` to retrieve this provided config.
+ * The config will only be provided once it has a non-null value.
+ * Subsequent updates to the config (including to undefined) will be reflected
+ * in the provided reference, but will not trigger a new provide.
  */
-export function useProvideConfigT<T>(config: MaybeRefOrGetter<T>) {
-  const configRef = ref(toValue(config)) as Ref<T>;
+export function useProvideConfigT<T>(
+  config: Exclude<MaybeRefOrGetter<T | null>, null>
+) {
+  const configRef = ref<T>() as Ref<T>;
+  let provided = false;
+
   watchEffect(() => {
-    configRef.value = toValue(config);
+    const value = toValue(config);
+    if (value === null) return;
+    configRef.value = value;
+    if (provided) return;
+    provide(injectionKey as InjectionKey<Ref<T>>, configRef);
+    provided = true;
   });
-  provide(injectionKey as InjectionKey<Ref<T>>, configRef);
 }
