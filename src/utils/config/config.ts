4195d996747c1fbc22a906a51ecc4bdc65b8a13b
Tai Sakuma <tai.sakuma@gmail.com>
2024-08-01 14:56:18 -0400

    Add `seedColor` to config

--- src/utils/config/config.ts
+++ src/utils/config/config.ts
@@ -2,6 +2,7 @@ export interface Config {
   appName: string;
   apiName: string;
   apiHttp: string;
+  seedColor: string;
 }
 
 export const defaultConfig = {
@@ -18,4 +19,8 @@ export function validateConfig(config: Config) {
 
   if (typeof config.apiHttp !== "string") throw Error("apiHttp is not string");
   if (config?.apiHttp === "") throw Error("apiHttp is empty");
+
+  if (typeof config.seedColor !== "string") throw Error("seedColor is not string");
+  if (!/^#([0-9A-F]{3}|[0-9A-F]{6}|[0-9A-F]{8})$/i.test(config.seedColor))
+    throw Error(`seedColor is not a valid hex color: ${config.seedColor}`);
 }
