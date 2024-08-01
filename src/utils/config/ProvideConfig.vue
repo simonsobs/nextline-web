51e11eb0f633098cf70994a977e556c3db077bed
Tai Sakuma <tai.sakuma@gmail.com>
2024-08-01 14:45:12 -0400

    Clean code in `ProvideConfig.vue`

--- src/utils/config/ProvideConfig.vue
+++ src/utils/config/ProvideConfig.vue
@@ -15,28 +15,8 @@
 /**
  * Load config asynchronously and provide it to the child components.
  */
-import { ref, watchEffect, nextTick } from "vue";
-import type { ComputedRef } from "vue";
 import { useLoadConfig } from "@/utils/config";
-import { useProvideConfig, Config } from "@/utils/config";
-
-const error = ref<Error>();
-
-const { error: loadError, config } = await useLoadConfig();
-watchEffect(() => {
-  error.value = loadError.value;
-});
-
-if (!config.value) throw new Error("Config is null");
-useProvideConfig(config as ComputedRef<Config>);
-
-// For test reactivity of loading.
-// await new Promise((resolve) => setTimeout(resolve, 1000));
-
-// For test reactivity of error.
-// error.value = new Error("Test");
-// nextTick(async () => {
-//   await new Promise((resolve) => setTimeout(resolve, 1000));
-//   error.value = undefined;
-// });
+import { useProvideConfig } from "@/utils/config";
+const { error, config } = await useLoadConfig();
+useProvideConfig(config);
 </script>
