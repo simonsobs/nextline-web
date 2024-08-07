573746ce1dd14e4579e9b98767b487e7431e537d
Tai Sakuma <tai.sakuma@gmail.com>
2024-08-07 11:03:12 -0400

    Extract `useArgb()` from `useDynamicScheme()`

--- src/utils/dynamic-color/dynamic-scheme.ts
+++ src/utils/dynamic-color/dynamic-scheme.ts
@@ -1,5 +1,5 @@
 import { computed, ref, watchEffect } from "vue";
-import type { MaybeRef, UnwrapRef } from "vue";
+import type { MaybeRef, Ref, UnwrapRef } from "vue";
 import { argbFromHex, Hct } from "@material/material-color-utilities";
 import { SchemeNameMap } from "./schemes";
 import type { SchemeName } from "./schemes";
@@ -20,27 +20,34 @@ const optDefault: Required<UnwrapRef<UseDynamicSchemeOptions>> = {
 
 // "#6750A4" is the default seed in https://www.figma.com/community/file/1035197037666593721/Visualizing-dynamic-color-in-your-app-with-Material-Design
 
-export function useDynamicScheme(options?: UseDynamicSchemeOptions) {
-  const schemeName = ref(options?.schemeName ?? optDefault.schemeName);
-  const sourceColorHex = ref(options?.sourceColorHex ?? optDefault.sourceColorHex);
-  const dark = ref(options?.dark ?? optDefault.dark);
-  const contrastLevel = ref(options?.contrastLevel ?? optDefault.contrastLevel);
-  const schemeClass = computed(() => SchemeNameMap[schemeName.value]);
-  const argb = ref(argbFromHex(optDefault.sourceColorHex));
+function useArgb(sourceColorHex: Ref<string>, defaultColorHex: string) {
+  const argb = ref(argbFromHex(defaultColorHex));
+
   watchEffect(
     () => {
       try {
         argb.value = argbFromHex(sourceColorHex.value);
       } catch (e: unknown) {
         const errorText = e instanceof Error ? e.message : `${e}`;
-        const message = `${errorText}; use default color: ${optDefault.sourceColorHex}`;
+        const message = `${errorText}; use default color: ${defaultColorHex}`;
         console.warn(message);
-        sourceColorHex.value = optDefault.sourceColorHex;
-        argb.value = argbFromHex(sourceColorHex.value);
+        sourceColorHex.value = defaultColorHex;
+        argb.value = argbFromHex(defaultColorHex);
       }
     },
     { flush: "sync" }
   );
+
+  return argb;
+}
+
+export function useDynamicScheme(options?: UseDynamicSchemeOptions) {
+  const schemeName = ref(options?.schemeName ?? optDefault.schemeName);
+  const sourceColorHex = ref(options?.sourceColorHex ?? optDefault.sourceColorHex);
+  const dark = ref(options?.dark ?? optDefault.dark);
+  const contrastLevel = ref(options?.contrastLevel ?? optDefault.contrastLevel);
+  const schemeClass = computed(() => SchemeNameMap[schemeName.value]);
+  const argb = useArgb(sourceColorHex, optDefault.sourceColorHex);
   const sourceColorHct = computed(() => Hct.fromInt(argb.value));
   const scheme = computed(
     () => new schemeClass.value(sourceColorHct.value, dark.value, contrastLevel.value)
