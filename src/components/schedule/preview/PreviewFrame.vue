e9d35062d14c6417af39212096410739852756d3
Tai Sakuma <tai.sakuma@gmail.com>
2025-01-08 12:52:28 -0500

    Add the route `/preview`

---
+++ src/components/schedule/preview/PreviewFrame.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="g-container">Preview</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.g-container {
+  position: relative;
+  display: grid;
+  overflow-y: auto;
+  block-size: 100%;
+  padding-bottom: calc(64px + 24px); /* 64px is the height of the fab button */
+  justify-content: center;
+  grid-template-columns: 1fr;
+  grid-template-rows: min-content min-content 1fr;
+  grid-template-areas: "breadcrumb" "top" "table";
+}
+</style>
---
+++ src/components/schedule/preview/PreviewView.vue
@@ -0,0 +1,7 @@
+<template>
+  <PreviewFrame></PreviewFrame>
+</template>
+
+<script setup lang="ts">
+import PreviewFrame from "./PreviewFrame.vue";
+</script>
