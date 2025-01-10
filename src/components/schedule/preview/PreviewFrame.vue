473932c33b0c8045e097084123916ea435395217
Tai Sakuma <tai.sakuma@gmail.com>
2025-01-10 15:47:15 -0500

    Add scheduler preview

--- src/components/schedule/preview/PreviewFrame.vue
+++ src/components/schedule/preview/PreviewFrame.vue
@@ -4,23 +4,45 @@
       <VBreadcrumbs :items="breadcrumb"> </VBreadcrumbs>
     </div>
     <div class="g-top">
-      <VBtn variant="flat" color="primary-fixed"> preview </VBtn>
+      <VBtn variant="flat" color="primary-fixed" :disabled="loading" @click="load">
+        preview
+      </VBtn>
+      <VProgressCircular
+        indeterminate
+        size="25"
+        color="primary"
+        v-if="loading"
+      ></VProgressCircular>
+    </div>
+    <div class="g-body" v-if="!loading">
+      <VAlert type="error" variant="tonal" v-if="error">
+        <pre
+          v-text="error"
+          style="white-space: pre-wrap; overflow-wrap: anywhere"
+        ></pre>
+      </VAlert>
+      <editor :source="script" v-else-if="script"> </editor>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { usePreview } from "./preview";
+import Editor from "./Editor.vue";
 const breadcrumb = [{ title: "Scheduler Preview", disabled: false }];
+const { script, loading, error, load } = usePreview();
 </script>
 
 <style scoped>
 .g-container {
+  block-size: 100%;
   display: grid;
   padding: 12px;
+  row-gap: 24px;
   justify-content: center;
   grid-template-columns: minmax(min-content, 960px);
   grid-template-rows: min-content min-content 1fr;
-  grid-template-areas: "breadcrumb" "top" "table";
+  grid-template-areas: "breadcrumb" "top" "body";
 }
 
 .g-breadcrumb {
@@ -32,5 +54,13 @@ const breadcrumb = [{ title: "Scheduler Preview", disabled: false }];
 
 .g-top {
   grid-area: top;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.g-body {
+  grid-area: body;
+  block-size: 100%;
 }
 </style>
