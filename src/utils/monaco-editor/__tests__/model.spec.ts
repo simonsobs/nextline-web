67f8029f6da3f90b6c6a6f52fe390c72beaa4c4f
Tai Sakuma <tai.sakuma@gmail.com>
2025-03-11 10:24:34 -0400

    Update tests

--- src/utils/monaco-editor/__tests__/model.spec.ts
+++ src/utils/monaco-editor/__tests__/model.spec.ts
@@ -1,10 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { ref, nextTick, unref, isRef } from "vue";
 
-import { until } from "@vueuse/core";
 import fc from "fast-check";
-
-import { withAsyncSetup } from "@/tests/test-utils";
 
 import { useModel } from "..";
 
