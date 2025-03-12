16e79494435006316b8f317320c94781d4252dca
Tai Sakuma <tai.sakuma@gmail.com>
2025-03-12 18:13:17 -0400

    Update `eslint.config.ts` for `eslint-plugin-import`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,12 @@
 import js from "@eslint/js";
-import * as tseslint from "typescript-eslint";
-import vuePlugin from "eslint-plugin-vue";
-import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
-import vueParser from "vue-eslint-parser"
-
-// @ts-ignore
+// @ts-expect-error No types available
 import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import vuePlugin from "eslint-plugin-vue";
+import * as tseslint from "typescript-eslint";
+// eslint-disable-next-line import/order
+import vueParser from "vue-eslint-parser";
 
 const isProduction = import.meta.env?.PROD || process.env.NODE_ENV === "production";
 
@@ -19,10 +19,61 @@ export default [
   {
     files: ["**/*.{js,ts,vue}"],
     settings: {
+      "import/internal-regex": "^package/",
       "import/resolver": {
         typescript: true,
         node: true,
       },
+    },
+    rules: {
+      // Import sorting configuration
+      "import/order": [
+        "error",
+        {
+          distinctGroup: false,
+          groups: [
+            "builtin", // Node.js built-in modules
+            "external", // Packages from node_modules
+            "internal", // From the project using aliases (@/...)
+            "parent", // From parent directories (../)
+            "sibling", // From the same directory (./)
+            "index", // From the same file
+          ],
+          pathGroups: [
+            {
+              pattern: "fast-check",
+              group: "external",
+              position: "after",
+            },
+            {
+              pattern: "vitest",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "vue",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          // Set `pathGroupsExcludedImportTypes` to `[]` to remove the default value of
+          // ['builtin', 'external', 'internal'] so that `pathGroups` work on `external`.
+          // https://github.com/import-js/eslint-plugin-import/blob/v2.31.0/docs/rules/order.md
+          pathGroupsExcludedImportTypes: [],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+          "newlines-between": "always",
+        },
+      ],
+
+      // Separate type imports
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
     },
   },
 
