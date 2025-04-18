58ee6d8f00a926887c201db5a2a0306dd28b05a9
Tai Sakuma <tai.sakuma@gmail.com>
2025-04-18 11:52:24 -0400

    Set the the height of the monaco editor from the content

--- src/components/rdb/Editor.vue
+++ src/components/rdb/Editor.vue
@@ -1,9 +1,9 @@
 <template>
-  <div ref="element" style="height: 100%"></div>
+  <div ref="element" :style="{ height: editorHeight }"></div>
 </template>
 
 <script setup lang="ts">
-import { ref, toRefs } from "vue";
+import { computed, ref, toRefs } from "vue";
 
 import { useMonacoEditor } from "@/utils/monaco-editor";
 
@@ -13,6 +13,15 @@ interface Props {
 const props = defineProps<Props>();
 const { source } = toRefs(props);
 const element = ref<HTMLElement>();
+
+const nLines = computed(() => source.value.split("\n").length);
+const lineHeight = 24; // From monaco-editor
+const padding = 10; // TODO: Check if this is correct
+const editorHeight = computed(() => {
+  const height = nLines.value * lineHeight + padding;
+  return `${height}px`;
+});
+
 // await new Promise((resolve) => setTimeout(resolve, 2000));
 await useMonacoEditor({ element, source });
 </script>
