7b4aa174c88783ee1edc8c6e1e2a5bd34c36ed93
Tai Sakuma <tai.sakuma@gmail.com>
2025-08-29 11:51:32 -0400

    Add comment and test to `onReady()`

--- src/utils/on-ready.ts
+++ src/utils/on-ready.ts
@@ -1,5 +1,18 @@
 export type OnReady<T extends object> = T & PromiseLike<T>;
 
+/**
+ * Makes a sync function awaitable by adding a `then` method to its return value.
+ *
+ * This function makes Vue composables optionally awaitable.
+ *
+ * Note: This function is inspired by VueUse, e.g.,
+ * https://github.com/vueuse/vueuse/blob/v13.8.0/packages/core/useFetch/index.ts#L639-L645
+ *
+ * @param ret - The return value without `then`
+ * @param ready - A promise that resolves when the `ret` is ready.
+ * @returns The `ret` with a `then` method.
+ *
+ */
 export function onReady<T extends object>(ret: T, ready: Promise<unknown>): OnReady<T> {
   return {
     ...ret,
@@ -8,4 +21,42 @@ export function onReady<T extends object>(ret: T, ready: Promise<unknown>): OnRe
       return Promise.resolve(ret).then(onFulfilled, onRejected);
     },
   };
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  function useFoo() {
+    const foo = { value: "bar" };
+
+    async function update() {
+      await Promise.resolve();
+      foo.value = "baz";
+    }
+    const ready = update();
+
+    const ret = { foo };
+    return onReady(ret, ready);
+  }
+
+  it("Should the composable be awaitable", async () => {
+    const { foo: bar } = await useFoo();
+    expect(bar.value).toEqual("baz");
+  });
+
+  it("Should the return value be updated later", async () => {
+    const ret = useFoo();
+    expect(ret.foo.value).toEqual("bar");
+
+    await ret; // Note: thenable. No need to call `then()`
+    expect(ret.foo.value).toEqual("baz");
+  });
+
+  it("Should `then()` be awaitable", async () => {
+    const { foo, then } = useFoo();
+    expect(foo.value).toEqual("bar");
+
+    await then(); // Note: `then` needs to be called.
+    expect(foo.value).toEqual("baz");
+  });
 }
