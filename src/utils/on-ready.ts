3c5914b03307e162504856a433e9bd59794f4659
Tai Sakuma <tai.sakuma@gmail.com>
2025-08-29 10:29:12 -0400

    Update type

--- src/utils/on-ready.ts
+++ src/utils/on-ready.ts
@@ -1,6 +1,6 @@
-export type OnReady<T> = T & PromiseLike<T>;
+export type OnReady<T extends object> = T & PromiseLike<T>;
 
-export function onReady<T>(ret: T, ready: Promise<unknown>): OnReady<T> {
+export function onReady<T extends object>(ret: T, ready: Promise<unknown>): OnReady<T> {
   return {
     ...ret,
     async then(onFulfilled, onRejected) {
