3177ff0f8f379d224a3cd07d30a28153ad2e80a1
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-04 19:11:02 -0400

    Move `useQueryBackedSubscription()` to a new file

---
+++ src/api/use-query-backed-subscription.ts
@@ -0,0 +1,30 @@
+import { computed } from "vue";
+import type { Ref } from "vue";
+import type { UseQueryResponse, UseSubscriptionResponse } from "@urql/vue";
+
+type QueryData<Q> = Q extends UseQueryResponse<infer Data, any> ? Data : never;
+type SubscriptionData<S> =
+  S extends UseSubscriptionResponse<infer Data, any, any> ? Data : never;
+
+export function useQueryBackedSubscription<
+  T,
+  Q extends UseQueryResponse<any, any>,
+  S extends UseSubscriptionResponse<any, any, any>,
+>(
+  query: Q,
+  subscription: S,
+  mapQueryData: (queryData: Ref<QueryData<Q>>) => T | undefined,
+  mapSubscriptionData: (
+    subscriptionData: Ref<SubscriptionData<S> | undefined>,
+  ) => T | undefined,
+) {
+  const error = computed(() => subscription.error?.value || query.error?.value);
+
+  const data = computed(() =>
+    error.value
+      ? undefined
+      : mapSubscriptionData(subscription.data) || mapQueryData(query.data),
+  );
+
+  return { data, error };
+}
--- src/api/use-state-subscription.ts
+++ src/api/use-state-subscription.ts
@@ -1,4 +1,3 @@
-import { computed } from "vue";
 import type { ComputedRef, Ref } from "vue";
 import type { UseQueryResponse, UseSubscriptionResponse } from "@urql/vue";
 
@@ -15,32 +14,7 @@ import type {
 import { onReady } from "@/utils/on-ready";
 import type { OnReady } from "@/utils/on-ready";
 
-type QueryData<Q> = Q extends UseQueryResponse<infer Data, any> ? Data : never;
-type SubscriptionData<S> =
-  S extends UseSubscriptionResponse<infer Data, any, any> ? Data : never;
-
-function useQueryBackedSubscription<
-  T,
-  Q extends UseQueryResponse<any, any>,
-  S extends UseSubscriptionResponse<any, any, any>,
->(
-  query: Q,
-  subscription: S,
-  mapQueryData: (queryData: Ref<QueryData<Q>>) => T | undefined,
-  mapSubscriptionData: (
-    subscriptionData: Ref<SubscriptionData<S> | undefined>,
-  ) => T | undefined,
-) {
-  const error = computed(() => subscription.error?.value || query.error?.value);
-
-  const data = computed(() =>
-    error.value
-      ? undefined
-      : mapSubscriptionData(subscription.data) || mapQueryData(query.data),
-  );
-
-  return { data, error };
-}
+import { useQueryBackedSubscription } from "./use-query-backed-subscription";
 
 type Query = UseQueryResponse<CtrlStateQuery, CtrlStateQueryVariables>;
 
