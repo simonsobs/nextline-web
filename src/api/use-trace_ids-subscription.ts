749d45d08443ae48d956433cb0829ed7f3ac3209
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-06 16:27:34 -0400

    Rewrite `useSubscribeTraceIds()`

--- src/api/use-trace_ids-subscription.ts
+++ src/api/use-trace_ids-subscription.ts
@@ -1,38 +1,30 @@
 import type { ComputedRef } from "vue";
-import { computed } from "vue";
 
 import {
   useCtrlTraceIdsQuery,
   useCtrlTraceIdsSSubscription,
 } from "@/graphql/codegen/generated";
-import { onReady } from "@/utils/on-ready";
-import type { OnReady } from "@/utils/on-ready";
+
+import { useQueryBackedSubscription } from "./use-query-backed-subscription";
 
 interface _TraceIdsSubscription {
   data: ComputedRef<number[] | undefined>;
   error: ComputedRef<Error | undefined>;
-  subscription: ReturnType<typeof useCtrlTraceIdsSSubscription>;
-  query: ReturnType<typeof useCtrlTraceIdsQuery>;
 }
 
-type TraceIdsSubscription = OnReady<_TraceIdsSubscription>;
+type TraceIdsSubscription = _TraceIdsSubscription & PromiseLike<_TraceIdsSubscription>;
 
 export function useSubscribeTraceIds(): TraceIdsSubscription {
-  const query = useCtrlTraceIdsQuery({
-    requestPolicy: "network-only",
-    variables: {},
-  });
+  const query = useCtrlTraceIdsQuery({ requestPolicy: "network-only", variables: {} });
   const subscription = useCtrlTraceIdsSSubscription({ variables: {} });
 
-  const error = computed(() => subscription.error?.value || query.error?.value);
+  const mapQueryData = (d: typeof query.data) => d.value?.ctrl.traceIds;
+  const mapSubscriptionData = (d: typeof subscription.data) => d.value?.ctrlTraceIds;
 
-  const data = computed(() =>
-    error.value
-      ? undefined
-      : subscription.data?.value?.ctrlTraceIds || query.data?.value?.ctrl.traceIds,
-  );
-
-  const ret = { data, error, subscription, query };
-
-  return onReady(ret, query);
+  return useQueryBackedSubscription({
+    query,
+    subscription,
+    mapQueryData,
+    mapSubscriptionData,
+  });
 }
