89bb87811c3bb3ffa5355385a38a13fb8d188038
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-06 15:33:52 -0400

    Extract `runPropertyTest`()

---
+++ src/api/__tests__/run-property-test.ts
@@ -0,0 +1,59 @@
+import { vi, expect } from "vitest";
+import type { ComputedRef } from "vue";
+import { useQuery, useSubscription } from "@urql/vue";
+import type { UseQueryResponse, UseSubscriptionResponse } from "@urql/vue";
+import fc from "fast-check";
+
+import { fcMockUseQueryResponseArg, fcMockUseSubscriptionResponseArg } from "./fc";
+import { mockUseQueryResponse } from "./mock-use-query-response";
+import { mockUseSubscriptionResponse } from "./mock-use-subscription-response";
+
+interface _UseSubscribeXXXReturn<R> {
+  data: ComputedRef<R | undefined>;
+  error: ComputedRef<Error | undefined>;
+}
+
+type UseSubscribeXXXReturn<R> = _UseSubscribeXXXReturn<R> &
+  PromiseLike<_UseSubscribeXXXReturn<R>>;
+
+export async function runPropertyTest<QueryData, SubData, R>(
+  useSubscribeXXX: () => UseSubscribeXXXReturn<R>,
+  mapQuery: (d: QueryData | undefined) => R | undefined,
+  mapSub: (d: SubData | undefined) => R | undefined,
+  fcQueryData: fc.Arbitrary<QueryData>,
+  fcSubData: fc.Arbitrary<SubData>,
+) {
+  type QueryRes = UseQueryResponse<QueryData, any>;
+  type SubRes = UseSubscriptionResponse<SubData, SubData, any>;
+
+  const fcQueryArg = fcMockUseQueryResponseArg(fcQueryData);
+  const fcSubArg = fcMockUseSubscriptionResponseArg(fcSubData);
+
+  await fc.assert(
+    fc.asyncProperty(fcQueryArg, fcSubArg, async (queryArg, subArg) => {
+      // Mock useGenQuery()
+      const queryRes = mockUseQueryResponse<QueryData>(queryArg);
+      vi.mocked(useQuery<QueryData>).mockReturnValue(queryRes as QueryRes);
+
+      // Mock useGenSub()
+      const { response: subRes, issue } = mockUseSubscriptionResponse<SubData>(subArg);
+      vi.mocked(useSubscription<SubData>).mockReturnValue(subRes as SubRes);
+
+      const { data, error } = await useSubscribeXXX();
+
+      // Assert initial values are from query.
+      expect(error.value).toBe(queryArg.error);
+      expect(data.value).toBe(queryArg.error ? undefined : mapQuery(queryArg.data));
+
+      // Assert the subsequent values are issued from subscription backed up by query.
+      for (const issued of issue) {
+        const expectedError = issued.error || queryArg.error;
+        const expectedData = expectedError
+          ? undefined
+          : (mapSub(issued.data) ?? mapQuery(queryArg.data));
+        expect(error.value).toBe(expectedError);
+        expect(data.value).toBe(expectedData);
+      }
+    }),
+  );
+}
--- src/api/__tests__/use-state-subscription.spec.ts
+++ src/api/__tests__/use-state-subscription.spec.ts
@@ -1,6 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { useQuery, useSubscription } from "@urql/vue";
-import type { UseQueryResponse, UseSubscriptionResponse } from "@urql/vue";
+import { it, vi } from "vitest";
 import fc from "fast-check";
 
 import type {
@@ -10,16 +8,13 @@ import type {
 
 import { useSubscribeState } from "../use-state-subscription";
 
-import { fcMockUseQueryResponseArg, fcMockUseSubscriptionResponseArg } from "./fc";
-import { mockUseQueryResponse } from "./mock-use-query-response";
-import { mockUseSubscriptionResponse } from "./mock-use-subscription-response";
+import { runPropertyTest } from "./run-property-test";
 
+// Mock functions used in runPropertyTest()
 vi.mock("@urql/vue", () => ({
   useQuery: vi.fn(),
   useSubscription: vi.fn(),
 }));
-
-const useSubscribeXXX = useSubscribeState;
 
 type QueryData = CtrlStateQuery;
 type SubData = CtrlStateSSubscription;
@@ -33,49 +28,6 @@ const fcQueryData: fc.Arbitrary<QueryData> = fc.record({
 });
 const fcSubData: fc.Arbitrary<SubData> = fc.record({ ctrlState: fcCtrlState });
 
-type QueryRes = UseQueryResponse<QueryData, any>;
-type SubRes = UseSubscriptionResponse<SubData, SubData, any>;
-
-const fcQueryArg = fcMockUseQueryResponseArg(fcQueryData);
-const fcSubArg = fcMockUseSubscriptionResponseArg(fcSubData);
-
-describe("useSubscribeXXX()", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
-
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
-
-  it("Property test", async () => {
-    await fc.assert(
-      fc.asyncProperty(fcQueryArg, fcSubArg, async (queryArg, subArg) => {
-        // Mock useGenQuery()
-        const queryRes = mockUseQueryResponse<QueryData>(queryArg);
-        vi.mocked(useQuery<QueryData>).mockReturnValue(queryRes as QueryRes);
-
-        // Mock useGenSub()
-        const { response: subRes, issue } =
-          mockUseSubscriptionResponse<SubData>(subArg);
-        vi.mocked(useSubscription<SubData>).mockReturnValue(subRes as SubRes);
-
-        const { data, error } = await useSubscribeXXX();
-
-        // Assert initial values are from query.
-        expect(error.value).toBe(queryArg.error);
-        expect(data.value).toBe(queryArg.error ? undefined : mapQuery(queryArg.data));
-
-        // Assert the subsequent values are issued from subscription backed up by query.
-        for (const issued of issue) {
-          const expectedError = issued.error || queryArg.error;
-          const expectedData = expectedError
-            ? undefined
-            : (mapSub(issued.data) ?? mapQuery(queryArg.data));
-          expect(error.value).toBe(expectedError);
-          expect(data.value).toBe(expectedData);
-        }
-      }),
-    );
-  });
+it("useSubscribeState", async () => {
+  await runPropertyTest(useSubscribeState, mapQuery, mapSub, fcQueryData, fcSubData);
 });
