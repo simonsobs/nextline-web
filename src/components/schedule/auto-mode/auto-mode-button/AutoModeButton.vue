d0a84811ce62a55809c3644c243b06a8ba358d77
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-25 12:58:27 -0400

    Remove `<VCardActions>` from `AutoModeButton.vue`

--- src/components/schedule/auto-mode/auto-mode-button/AutoModeButton.vue
+++ src/components/schedule/auto-mode/auto-mode-button/AutoModeButton.vue
@@ -1,11 +1,9 @@
 <template>
-  <VCardActions>
-    <VBtn v-if="loading" loading variant="tonal" color="tertiary"> </VBtn>
-    <ButtonError v-else-if="error" :error="error"></ButtonError>
-    <ButtonAutoMode v-else-if="autoMode"></ButtonAutoMode>
-    <ButtonOff v-else-if="autoMode === false"></ButtonOff>
-    <VBtn v-else variant="tonal" color="error"> Unknown </VBtn>
-  </VCardActions>
+  <VBtn v-if="loading" loading variant="tonal" color="tertiary"> </VBtn>
+  <ButtonError v-else-if="error" :error="error"></ButtonError>
+  <ButtonAutoMode v-else-if="autoMode"></ButtonAutoMode>
+  <ButtonOff v-else-if="autoMode === false"></ButtonOff>
+  <VBtn v-else variant="tonal" color="error"> Unknown </VBtn>
 </template>
 
 <script setup lang="ts">
